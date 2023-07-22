8dd4af88ae71a12e255ac557902695476f8811e5
donvine mugendi <donvinemugendi@gmail.com>
2023-07-23 02:26:40 +0300

    updated menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -6,7 +6,7 @@ import { GoSignOut } from "react-icons/go";
 
 export const Menu = ({ handleClick, menuOpen }) => {
   return (
-    <nav className="absolute left-0 top-1 space-y-10 mt-3 md:mt-5 dark:bg-gray-900 shadow-lg bg-slate-100 py-4 rounded-lg md:w-60 w-2/3 z-50">
+    <nav className="absolute left-0 top-1 space-y-10 mt-3 md:mt-5 dark:bg-gray-900 shadow-lg bg-slate-100 py-4 rounded-lg md:w-60 w-2/3 z-50 h-screen">
       <h1
         className="absolute top-0 right-0 text-4xl mr-2 hover:scale-125 ease-in-out cursor-pointer"
         onClick={() => handleClick(!menuOpen)}>
@@ -41,6 +41,11 @@ export const Menu = ({ handleClick, menuOpen }) => {
         <GoSignOut />
         Sign Out
       </Link>
+      <div className="hidden md:block px-3 py-2 mt-0">
+         <p className="font-bold"><span className="font-xl">Tech Tales</span> Is a community of over 1000 amazing tech students passionate about technology</p>
+        <p className="text-sm text-gray-200 py-2">We are a place where coders share, stay up-to-date and grow their careers.</p>
+      </div>
+
     </nav>
   );
 };
