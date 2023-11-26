d26e1b465868c5db85e970fd48129c39a084bc12
Important Person <126117678+Donvine254@users.noreply.github.com>
2023-11-27 02:49:24 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { BlogsComponent } from "@/components";
 const url = "https://techtales.up.railway.app/blogs";
 export default function Page() {
   return (
-    <section className="relative">
+    <section className="relative md:min-h-[350px]">
       <BlogsComponent blogsUrl={url} />
     </section>
   );
