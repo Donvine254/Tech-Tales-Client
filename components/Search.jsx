663121b8851617fd37ce637dc266cbf2ab024ae4
Important Person <126117678+Donvine254@users.noreply.github.com>
2023-12-04 18:36:39 +0300

    add prevent default to search form

--- components/Search.jsx
+++ components/Search.jsx
@@ -7,6 +7,7 @@ export const Search = () => {
   const router = useRouter();
   const [query, setQuery] = useState("");
   function handleSearch(e) {
+    e.preventDefault()
     const value = query.trim();
     router.replace(`/?search=${value}`);
   }
