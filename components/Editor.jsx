19449ac8eb608da731e38685b4680c5459f01901
Donvine Mugendi <donvinemugendi@gmail.com>
2023-12-06 17:38:57 +0300

    add tiny mce editor

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,11 +1,53 @@
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-
-export default function Editor() {
-  
-
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
   return (
-    <div
-      id="editorjs"
-      className=" text-black text-xl h-[200px] bg-base-100"></div>
+    <div>
+      <Editor
+        apiKey="s91bfina8wqeldhbyfzpf16bkodlx2gz2s0nx1zg9dfugp3e"
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>Write Your Blog Here....</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+
+      <button onClick={log}>Log editor content</button>
+    </div>
   );
 }
