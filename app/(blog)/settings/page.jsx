f82fd1fe1615fb026235ee66b3d8d6dcef7f9428
Donvine Mugendi <donvinemugendi@gmail.com>
2023-12-19 23:49:10 +0300

    update deactiviting user account

--- app/(blog)/settings/page.jsx
+++ app/(blog)/settings/page.jsx
@@ -1,18 +1,55 @@
 "use client";
 import React, { useEffect } from "react";
 import { getCurrentUser } from "@/lib";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import Image from "next/image";
+import Swal from "sweetalert2";
+import { clearCurrentUser, clearAllCookies } from "@/lib";
+import Loader from "@/components/Loader";
 
 export default function Page() {
   const user = getCurrentUser();
+  const navigate = useRouter();
   useEffect(() => {
     if (!user) {
       toast.error("kindly login first!");
       navigate.replace("/login");
     }
-  }, []);
-
+  }, [user]);
+  if (!user) {
+    return (
+      <div className="md:min-h-[400px]">
+        <div className="flex items-center justify-center gap-2 text-xl my-2">
+          <Loader />
+          Loading Details...
+        </div>
+      </div>
+    );
+  }
+  //function to deactivate user account
+  function handleDeactivate() {
+    Swal.fire({
+      icon: "warning",
+      title: "Deactivate Account",
+      text: "Deactivating your account will remove it from TechTales within a few minutes. You can sign back in anytime to reactivate your account and restore its content.",
+      showCloseButton: true,
+      confirmButtonColor: "red",
+      confirmButtonText: "Deactivate",
+      showCancelButton: true,
+      cancelButtonColor: "green",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clearCurrentUser();
+        clearAllCookies();
+        navigate.replace("/");
+      }
+    });
+  }
+  //function to deleteAccount
+  function handleDeleteAccount() {
+    toast.error("stop this nonsense!");
+  }
   return (
     <div className="font-poppins flex items-center justify-center m-auto ">
       <div className="bg-slate-100 shadow border-2 py-2 rounded-md">
@@ -50,10 +87,14 @@ export default function Page() {
           </div>
         </div>
         <div className="items-center p-6 flex flex-col space-y-4">
-          <button className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 w-full bg-yellow-500 text-white rounded-md">
+          <button
+            className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 w-full bg-yellow-500 text-white rounded-md"
+            onClick={handleDeactivate}>
             Deactivate Account
           </button>
-          <button className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 w-full bg-red-500 text-white rounded-md">
+          <button
+            className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 w-full bg-red-500 text-white rounded-md"
+            onClick={handleDeleteAccount}>
             Delete Account
           </button>
         </div>
