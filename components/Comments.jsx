dda992503cf3497098f53743a3c540fe44fe8d5c
Donvine Mugendi <donvinemugendi@gmail.com>
2024-01-01 21:43:32 +0300

    updated undo editing

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -35,7 +35,7 @@ export default function Comments({ comments, setComments, blogId }) {
       })
       .then((data) => {
         setNewComment("");
-        setComments((prev) => ({ ...prev, ...data }));
+        setComments((prev) => [...prev, data]);
       })
       .catch((error) => console.error("Error posting comment:", error));
   }
@@ -45,40 +45,24 @@ export default function Comments({ comments, setComments, blogId }) {
     setIsEditing(true);
     setIsInputFocused(true);
     setNewComment(comment.body);
-    setComments((prev) => prev.filter((comment) => comment.id !== commentToEdit.id));
+    setComments((prev) =>
+      prev.filter((prevcomment) => prevcomment.id !== comment.id)
+    );
   }
   function handleUpdate(e) {
     e.preventDefault();
-    patchComment(commentToEdit.id, setComments, newComment);
+    patchComment(commentToEdit?.id, setComments, newComment);
     setNewComment("");
     setIsEditing(false);
   }
 
   function undoEditing() {
-    setIsEditing(false);
     setIsInputFocused(false);
-    setComments((prev) => [...prev, ...commentToEdit]);
+    setComments((prev) => [...prev, commentToEdit]);
     setNewComment("");
+    setIsEditing(false);
   }
 
-  if (!comments) {
-    return (
-      <div className="flex items-center gap-2 py-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 4335 4335"
-          width="30"
-          className="animate-spin mr-3"
-          height="30">
-          <path
-            fill="#008DD2"
-            d="M3346 1077c41,0 75,34 75,75 0,41 -34,75 -75,75 -41,0 -75,-34 -75,-75 0,-41 34,-75 75,-75zm-1198 -824c193,0 349,156 349,349 0,193 -156,349 -349,349 -193,0 -349,-156 -349,-349 0,-193 156,-349 349,-349zm-1116 546c151,0 274,123 274,274 0,151 -123,274 -274,274 -151,0 -274,-123 -274,-274 0,-151 123,-274 274,-274zm-500 1189c134,0 243,109 243,243 0,134 -109,243 -243,243 -134,0 -243,-109 -243,-243 0,-134 109,-243 243,-243zm500 1223c121,0 218,98 218,218 0,121 -98,218 -218,218 -121,0 -218,-98 -218,-218 0,-121 98,-218 218,-218zm1116 434c110,0 200,89 200,200 0,110 -89,200 -200,200 -110,0 -200,-89 -200,-200 0,-110 89,-200 200,-200zm1145 -434c81,0 147,66 147,147 0,81 -66,147 -147,147 -81,0 -147,-66 -147,-147 0,-81 66,-147 147,-147zm459 -1098c65,0 119,53 119,119 0,65 -53,119 -119,119 -65,0 -119,-53 -119,-119 0,-65 53,-119 119,-119z"
-          />
-        </svg>
-        <p className="text-2xl"> Loading Comments...</p>
-      </div>
-    );
-  }
   return (
     <section>
       <form className="mt-4">
