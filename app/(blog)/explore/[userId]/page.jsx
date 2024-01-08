a80e3785e62e6801efd968ad50d278f7624e8959
Donvine <donvinemugendi@gmail.com>
2024-01-09 02:28:44 +0300

    switch explore page to server side rendering

--- app/(blog)/explore/[userId]/page.jsx
+++ app/(blog)/explore/[userId]/page.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import MyBlogsComponent from "../../my-blogs/blogs";
+import BlogsComponent from "./blogs";
+export const revalidate = 600;
 export const metadata = {
   title: "Explore Author Blogs - Tech Tales",
   description:
     "Tech Tales is a simple school blog for software developers students and senior developers who would like to share their solutions to various coding problems or practice blogging as a way of learning",
 };
-export default function page({ params }) {
+export async function generateStaticParams() {
+  try {
+    const response = await fetch("https://techtales.up.railway.app/blogs", {
+      next: { revalidate: 600 },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    const data = await response.json();
+    // Extract blog IDs from the data and return as an array
+    if (!Array.isArray(data)) {
+      throw new Error("Invalid data format: expected an array.");
+    }
+
+    const userIdsSet = new Set(data.map((blog) => blog.user_id));
+    return Array.from(userIdsSet);
+  } catch (error) {
+    console.error("Error fetching blog data:", error);
+    return [];
+  }
+}
+export default async function page({ params }) {
+  const blogs = await fetch(
+    `https://techtales.up.railway.app/blogs/user/${params.userId}`,
+    {
+      next: { revalidate: 600 },
+    }
+  ).then((response) => response.json());
   return (
     <section>
-      <MyBlogsComponent id={params.userId} />
+      <BlogsComponent blogs={blogs} />
     </section>
   );
 }
