4039943db157e928c062af93b8fed811c544ce6a
Donvine Mugendi <donvinemugendi@gmail.com>
2024-01-11 02:20:03 +0300

    change className

--- app/(blog)/me/settings/page.jsx
+++ app/(blog)/me/settings/page.jsx
@@ -120,7 +120,7 @@ export default function Page() {
               setting is off.
             </p>
           </div>
-          <label class="relative inline-flex items-center cursor-pointer">
+          <label className="relative inline-flex items-center cursor-pointer">
             <input type="checkbox" value="" class="sr-only peer" />
             <div class="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-blue-600"></div>
           </label>
