835019f4dc659c0d1b8a5759a04c1675cdb6ed8c
Donvine <donvinemugendi@gmail.com>
2024-01-12 14:52:25 +0300

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,14 @@
 "use client";
 import Link from "next/link";
-import { Facebook, Twitter } from "@/assets";
+import { Facebook, GithubIcon, Twitter } from "@/assets";
 export default function Footer() {
   return (
-    <footer className="max-w-7xl w-full mx-auto m-5 p-4 divide-blue-500 font-poppins text-xl">
+    <footer className="max-w-7xl w-full mx-auto m-2 p-4 divide-blue-500 font-poppins text-xl">
       <h1 className="p-2 px-4">
-        <span className=" md:text-xl font-bold">Tech Tales</span>
+        <span className="md:text-xl font-bold">Tech Tales</span>
         <span className="xsm:text-sm">
-          - An inclusive social network blog for software developers
+          &#x2015; An inclusive social network blog for tech enthusiasts and
+          professionals
         </span>
       </h1>
       <div className="flex flex-wrap md:justify-between md:flex-row align-center md:gap-2">
@@ -32,7 +33,7 @@ export default function Footer() {
         <h1 className="text-xl">
           &copy; {new Date().getFullYear()} Tech Tales
         </h1>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center space-x-1">
           <Link
             href="https://www.facebook.com"
             className="navigation__footer"
@@ -49,20 +50,7 @@ export default function Footer() {
             href="https://github.com/Donvine254"
             className="navigation__footer"
             target="_blank">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="md:text-2xl">
-              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-              <path d="M9 18c-4.51 2-5-2-7-2" />
-            </svg>
+            <GithubIcon />
           </Link>
           <Link
             href="https://youtube.com/tech-tales"
