378007475035d3809752e0e640d20ac42be77fbe
Donvine <donvinemugendi@gmail.com>
2024-01-14 14:39:49 +0300

    update login alert

--- components/LoginAlert.jsx
+++ components/LoginAlert.jsx
@@ -52,7 +52,7 @@ export default function Popup({ setIsOpen }) {
       id="login_popup">
       <div className="p-4 max-w-md">
         <div className="flex justify-between items-center mb-4 ">
-          <h2 className="font-semibold text-[18px] text-gray-600 my-2">
+          <h2 className="font-medium text-[18px] xsm:text-base text-gray-500 my-2">
             Sign in to access personalized content, settings and more!
           </h2>
           <button
@@ -86,12 +86,7 @@ export default function Popup({ setIsOpen }) {
             href="/login?redirect=true&referrer=homepage"
             className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-gray-200  focus:outline-none"
             title="email-login">
-            <svg
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              height="24"
-              width="24"
-              className="h-6 w-6 mr-2">
+            <svg viewBox="0 0 24 24" fill="currentColor" height="24" width="24">
               <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6m-2 0l-8 5-8-5h16m0 12H4V8l8 5 8-5v10z" />
             </svg>
             Sign In with Email
