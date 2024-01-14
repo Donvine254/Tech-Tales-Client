76f7dfae2ace881ed392ede39ba0335145a9b505
Donvine <donvinemugendi@gmail.com>
2024-01-14 16:03:47 +0300

    update login with google button

--- components/LoginAlert.jsx
+++ components/LoginAlert.jsx
@@ -70,21 +70,21 @@ export default function Popup({ setIsOpen }) {
         <div className="flex flex-col space-y-2">
           <button
             onClick={handleGoogleLogin}
-            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-blue-600 hover:text-white focus:outline-none"
+            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-black hover:text-white focus:outline-none bg-gray-200 bg-opacity-20"
             title="google login">
             <GoogleIcon />
             Sign In with Google
           </button>
           <Link
             href="https://github.com/login/oauth/authorize?client_id=2384921712f034fd32cf"
-            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-black hover:text-white focus:outline-none"
+            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-black hover:text-white focus:outline-none bg-gray-200 bg-opacity-20"
             title="github login">
             <GithubIcon className="h-6 w-6 mr-2" />
             Sign In with Github
           </Link>
           <Link
             href="/login?redirect=true&referrer=homepage"
-            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-gray-200  focus:outline-none"
+            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md border text-black hover:bg-gray-200 bg-gray-200 bg-opacity-20 focus:outline-none"
             title="email-login">
             <svg viewBox="0 0 24 24" fill="currentColor" height="24" width="24">
               <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6m-2 0l-8 5-8-5h16m0 12H4V8l8 5 8-5v10z" />
