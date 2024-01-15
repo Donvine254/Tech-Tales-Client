8f65bdee34d1538a2b37b4dbd6561a9fc43d61eb
Donvine <donvinemugendi@gmail.com>
2024-01-15 21:54:01 +0300

    add community page

---
+++ app/(blog)/community/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+export const metadata = {
+  title: "Community Guidelines- Tech Tales",
+  description:
+    "Tech Tales is a simple blog for tech students and professionals who would like to share their solutions to various coding problems or practice blogging as a way of learning",
+};
+export default function page() {
+  return (
+    <section>
+      <h1 className="font-bold text-gray-600 leading-loose text-center text-xl md:text-2xl">
+        This is the community guidelines page
+      </h1>
+    </section>
+  );
+}
