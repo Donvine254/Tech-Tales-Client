4acfe4e7190da86df511abf1567e3f94b6947c73
Donvine <donvinemugendi@gmail.com>
2024-06-27 02:26:30 +0300

    update user card alignment

--- app/(blog)/me/page.jsx
+++ app/(blog)/me/page.jsx
@@ -99,7 +99,7 @@ export default function Profile() {
             className="w-[120px] h-[120px] rounded-full m-auto ring-offset-4 ring-2 ring-blue-600 ring-offset-white"
           />
 
-          <p className="text-gray-700 font-semibold my-2 flex items-center justify-center">
+          <p className="text-gray-700 font-semibold my-2 flex items-center justify-center ">
             <span>{user.username} </span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -110,7 +110,8 @@ export default function Profile() {
               stroke="white"
               strokeWidth="2"
               strokeLinecap="round"
-              strokeLinejoin="round">
+              strokeLinejoin="round"
+              className="mb-1">
               <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
               <path d="m9 12 2 2 4-4" />
             </svg>
--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -12,22 +12,24 @@ export default function UserCard({ avatar, name, userId, bio, title }) {
           alt={name}
           className="h-10 w-10 md:h-12 md:w-12 rounded-full cursor-pointer"
         />
-        <div className="capitalize font-bold text-lg inline-flex items-center">
-          <p className="w-full">{name}</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="#09A3E5"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="self-end">
-            <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-            <path d="m9 12 2 2 4-4" />
-          </svg>
+        <div className="capitalize font-bold text-lg ">
+          <p className="w-full inline-block relative pr-6">
+            <span className="relative">{name}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="#09A3E5"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="absolute top-0 right-0">
+              <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+              <path d="m9 12 2 2 4-4" />
+            </svg>
+          </p>
         </div>
       </div>
       <p className="text-base font-medium text-gray-600 my-1">
