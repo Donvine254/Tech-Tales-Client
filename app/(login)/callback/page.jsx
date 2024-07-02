32dadac0f8b014f48702a0926b28b54bc6f5897d
Donvine <donvinemugendi@gmail.com>
2024-07-02 19:38:41 +0300

    add suspsense to pages with useSearchParams

--- app/(login)/callback/page.jsx
+++ app/(login)/callback/page.jsx
@@ -5,6 +5,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import Axios from "axios";
 import { authenticateUser } from "@/lib";
 import Image from "next/image";
+import { Suspense } from "react";
 export default function AuthPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -25,38 +26,45 @@ export default function AuthPage() {
   }, [githubCode, router]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
-      <div className="flex flex-col items-center justify-center bg-white border p-6 rounded-md">
-        <div className="flex items-center gap-1 w-full overflow-hidden">
-          <Image
-            src="https://miro.medium.com/v2/resize:fit:1125/1*biIy42Cn4Bnu0IkpUW1Zew.png"
-            alt="github"
-            height={200}
-            width={200}
-            priority
-          />
-          <p className="text-6xl">+</p>
-          <Image
-            src="/logo.png"
-            height={50}
-            width={50}
-            alt="techtales"
-            priority
-          />
+    <Suspense
+      fallback={
+        <div>
+          <Loader />
         </div>
-        <p className="text-center font-medium text-gray-600 my-2">
-          Github is validating your identity.
-        </p>
-        <Loader size={60} fill="grey" />
-        <p className="text-base font-medium my-2 text-gray-600">
-          This taking too long? &nbsp;
-          <a
-            href="/login"
-            className="text-blue-600 font-bold border py-0.5 px-1 hover:bg-blue-600 hover:text-white rounded-md">
-            sign in another way
-          </a>
-        </p>
+      }>
+      <div className="flex items-center justify-center min-h-screen ">
+        <div className="flex flex-col items-center justify-center bg-white border p-6 rounded-md">
+          <div className="flex items-center gap-1 w-full overflow-hidden">
+            <Image
+              src="https://miro.medium.com/v2/resize:fit:1125/1*biIy42Cn4Bnu0IkpUW1Zew.png"
+              alt="github"
+              height={200}
+              width={200}
+              priority
+            />
+            <p className="text-6xl">+</p>
+            <Image
+              src="/logo.png"
+              height={50}
+              width={50}
+              alt="techtales"
+              priority
+            />
+          </div>
+          <p className="text-center font-medium text-gray-600 my-2">
+            Github is validating your identity.
+          </p>
+          <Loader size={60} fill="grey" />
+          <p className="text-base font-medium my-2 text-gray-600">
+            This taking too long? &nbsp;
+            <a
+              href="/login"
+              className="text-blue-600 font-bold border py-0.5 px-1 hover:bg-blue-600 hover:text-white rounded-md">
+              sign in another way
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
--- app/(login)/login/page.jsx
+++ app/(login)/login/page.jsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast";
 import { useGoogleLogin } from "@react-oauth/google";
 import Loader from "@/components/Loader";
 import { GithubIcon, GoogleIcon } from "@/assets";
+import { Suspense } from "react";
 
 export default function Page() {
   const [showPassword, setShowPassword] = useState(false);
@@ -61,111 +62,118 @@ export default function Page() {
   }
 
   return (
-    <form className="w-full" onSubmit={handleSubmit}>
-      <div className="flex flex-col items-center justify-center w-full min-h-screen px-4 font-poppins">
-        <div
-          className="border text-card-foreground w-full max-w-sm mx-auto rounded-xl shadow-md overflow-hidden bg-white"
-          data-v0-t="card">
-          <div className="flex flex-col space-y-1.5 px-6 py-4 font-poppins relative">
-            <h3 className="font-semibold tracking-tight text-2xl text-center">
-              Login to Your Account
-            </h3>
-            <p className="text-base  text-center">
-              Access your personalized settings and content.
-            </p>
-          </div>
-          <div className="px-6 space-y-2">
-            <div className="space-y-2 group">
-              <label
-                className="text-base font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-700"
-                htmlFor="email">
-                Email
-              </label>
-              <input
-                className="flex h-10 bg-background text-base  disabled:cursor-not-allowed disabled:opacity-50 w-full px-3 py-2 border border-gray-300 rounded-md"
-                id="email"
-                name="email"
-                type="email"
-                placeholder="you@example.com"
-                value={loginData.email}
-                onChange={handleChange}
-                required
-                disabled={loading}
-              />
+    <Suspense
+      fallback={
+        <div>
+          <Loader />
+        </div>
+      }>
+      <form className="w-full" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center justify-center w-full min-h-screen px-4 font-poppins">
+          <div
+            className="border text-card-foreground w-full max-w-sm mx-auto rounded-xl shadow-md overflow-hidden bg-white"
+            data-v0-t="card">
+            <div className="flex flex-col space-y-1.5 px-6 py-4 font-poppins relative">
+              <h3 className="font-semibold tracking-tight text-2xl text-center">
+                Login to Your Account
+              </h3>
+              <p className="text-base  text-center">
+                Access your personalized settings and content.
+              </p>
             </div>
-            <div className="space-y-2">
-              <label
-                className="text-base font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-700"
-                htmlFor="password">
-                Password
-              </label>
-              <input
-                className="flex h-10 bg-background text-base disabled:cursor-not-allowed disabled:opacity-50 w-full px-3 py-2 border border-gray-300 rounded-md"
-                id="password"
-                name="password"
-                placeholder="*******"
-                value={loginData.password}
-                onChange={handleChange}
-                minLength={8}
-                disabled={loading}
-                required
-                type={showPassword ? "text" : "password"}
-              />
-            </div>
-            <div className="xsm:text-sm flex items-center justify-between gap-1 md:gap-4">
-              <div className="flex items-center justify-start gap-1 md:gap-4">
+            <div className="px-6 space-y-2">
+              <div className="space-y-2 group">
+                <label
+                  className="text-base font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-700"
+                  htmlFor="email">
+                  Email
+                </label>
                 <input
-                  type="checkbox"
-                  value={showPassword}
-                  onChange={() => setShowPassword(!showPassword)}
+                  className="flex h-10 bg-background text-base  disabled:cursor-not-allowed disabled:opacity-50 w-full px-3 py-2 border border-gray-300 rounded-md"
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="you@example.com"
+                  value={loginData.email}
+                  onChange={handleChange}
+                  required
+                  disabled={loading}
                 />
-                <span> {!showPassword ? "Show" : "Hide"} Password</span>
               </div>
-              <div>
-                <a
-                  className="hover:text-blue-500 underline underline-offset-2 cursor-pointer"
-                  href="/reset?action=unsafe&not-recommended">
-                  Forgot Password?
-                </a>
+              <div className="space-y-2">
+                <label
+                  className="text-base font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-700"
+                  htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="flex h-10 bg-background text-base disabled:cursor-not-allowed disabled:opacity-50 w-full px-3 py-2 border border-gray-300 rounded-md"
+                  id="password"
+                  name="password"
+                  placeholder="*******"
+                  value={loginData.password}
+                  onChange={handleChange}
+                  minLength={8}
+                  disabled={loading}
+                  required
+                  type={showPassword ? "text" : "password"}
+                />
               </div>
+              <div className="xsm:text-sm flex items-center justify-between gap-1 md:gap-4">
+                <div className="flex items-center justify-start gap-1 md:gap-4">
+                  <input
+                    type="checkbox"
+                    value={showPassword}
+                    onChange={() => setShowPassword(!showPassword)}
+                  />
+                  <span> {!showPassword ? "Show" : "Hide"} Password</span>
+                </div>
+                <div>
+                  <a
+                    className="hover:text-blue-500 underline underline-offset-2 cursor-pointer"
+                    href="/reset?action=unsafe&not-recommended">
+                    Forgot Password?
+                  </a>
+                </div>
+              </div>
+              {errors && <ErrorList errors={errors} />}
             </div>
-            {errors && <ErrorList errors={errors} />}
-          </div>
 
-          <div className="items-center p-6 flex flex-col space-y-4">
-            <button
-              className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black  h-10 px-4 py-2 w-full bg-blue-500 hover:bg-blue-600 text-white rounded-md"
-              type="submit"
-              disabled={loading}
-              title="login">
-              {loading ? <Loader size={30} /> : "Sign in"}
-            </button>
-            {/* beginning of social logins */}
-            <button
-              className="rounded-md text-base font-medium  border  hover:bg-black hover:text-white  h-10 px-4 py-2 w-full flex justify-center items-center space-x-2"
-              type="button"
-              onClick={handleGoogleLogin}>
-              <GoogleIcon />
-              <span>Sign in with Google</span>
-            </button>
-            <button
-              className="rounded-md text-base font-medium  border  hover:bg-black hover:text-white  h-10 px-4 py-2 w-full flex justify-center items-center space-x-2"
-              type="button"
-              onClick={handleGithubLogin}>
-              <GithubIcon />
-              <span>Sign in with Github</span>
-            </button>
+            <div className="items-center p-6 flex flex-col space-y-4">
+              <button
+                className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black  h-10 px-4 py-2 w-full bg-blue-500 hover:bg-blue-600 text-white rounded-md"
+                type="submit"
+                disabled={loading}
+                title="login">
+                {loading ? <Loader size={30} /> : "Sign in"}
+              </button>
+              {/* beginning of social logins */}
+              <button
+                className="rounded-md text-base font-medium  border  hover:bg-black hover:text-white  h-10 px-4 py-2 w-full flex justify-center items-center space-x-2"
+                type="button"
+                onClick={handleGoogleLogin}>
+                <GoogleIcon />
+                <span>Sign in with Google</span>
+              </button>
+              <button
+                className="rounded-md text-base font-medium  border  hover:bg-black hover:text-white  h-10 px-4 py-2 w-full flex justify-center items-center space-x-2"
+                type="button"
+                onClick={handleGithubLogin}>
+                <GithubIcon />
+                <span>Sign in with Github</span>
+              </button>
+            </div>
+          </div>
+          <div className="mt-2 text-gray-600">
+            Not a member?{" "}
+            <a
+              className="text-blue-500 hover:underline border px-2 py-0.5"
+              href="register">
+              Register Here
+            </a>
           </div>
         </div>
-        <div className="mt-2 text-gray-600">
-          Not a member?{" "}
-          <a
-            className="text-blue-500 hover:underline border px-2 py-0.5"
-            href="register">
-            Register Here
-          </a>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Suspense>
   );
 }
