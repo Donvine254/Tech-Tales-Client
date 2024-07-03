9c43b9f0efbe203639441d5df174be8f6a66644a
Donvine <donvinemugendi@gmail.com>
2024-07-03 03:01:21 +0300

    update login route to set jwt cookies

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,65 +1,97 @@
 // pages/api/login.js
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/prisma/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function POST(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
   const data = await req.json();
-  console.log(data);
-  try {
-    // Check if the user exists
-    const user = await prisma.users.findUnique({
-      where: { email: data.email },
-    });
 
-    if (!user) {
-      return res.status(404).json({ errors: ["User not found"] });
+  if (data) {
+    try {
+      // Check if the user exists
+      const user = await prisma.users.findFirst({
+        where: { email: data?.email.toString() },
+      });
+
+      if (!user) {
+        return NextResponse.json(
+          { errors: ["User not found"] },
+          { status: 404 }
+        );
+      }
+
+      // Compare the password
+      const isPasswordValid = await bcrypt.compare(
+        data.password,
+        user.password_digest
+      );
+
+      if (!isPasswordValid) {
+        return NextResponse.json(
+          { errors: ["Invalid password, please try again"] },
+          { status: 401 }
+        );
+      }
+
+      // Generate a JWT token
+      const token = jwt.sign(
+        { userId: user.id.toString() },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "8h",
+        }
+      );
+      // set cookies
+      cookies().set({
+        name: "auth_token",
+        value: jwt,
+        httpOnly: true,
+      });
+      // Return user details and token
+      return NextResponse.json(
+        {
+          user: {
+            id: user.id.toString(),
+            email: user.email,
+            picture: user.picture,
+            socials: user.socials,
+            bio: user.bio,
+            role: user.role,
+          },
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        {
+          errors: ["Internal server error"],
+        },
+        { status: 500 }
+      );
+    } finally {
+      // Disconnect Prisma client
+      await prisma.$disconnect();
     }
-
-    // Compare the password
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      user.password_digest
-    );
-
-    if (!isPasswordValid) {
-      return res
-        .status(401)
-        .json({ errors: ["Invalid password, please try again."] });
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "8h",
-    });
-
-    // Return user details and token
-    return res.status(200).json({
-      user: {
-        id: user.id.toString(),
-        email: user.email,
-        picture: user.picture,
-        socials: user.socials,
-        bio: user.bio,
-        role: user.role,
+  } else {
+    return NextResponse.json(
+      {
+        errors: ["Internal server error"],
       },
-      token,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ errors: ["Internal server error"] });
-  } finally {
-    // Disconnect Prisma client
-    await prisma.$disconnect();
+      { status: 500 }
+    );
   }
 }
 
 export async function GET(req, res) {
-  return NextResponse.json({ error: "Not login parameters provided" });
+  return NextResponse.json(
+    { error: "Not login parameters provided" },
+    { status: 201 }
+  );
 }
