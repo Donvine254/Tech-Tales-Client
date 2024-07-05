8e302588e6ac71cb7ffc175af6e66fafcd5c49b7
Donvine <donvinemugendi@gmail.com>
2024-07-05 21:38:53 +0300

    comment out route.js

--- app/api/auth/oauth-login/route.js
+++ app/api/auth/oauth-login/route.js
@@ -1,67 +1,66 @@
-import prisma from "@/prisma/prisma";
-import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
+// import prisma from "@/prisma/prisma";
+// import jwt from "jsonwebtoken";
+// import { NextResponse } from "next/server";
 
-export async function POST(req, res) {
-  if (req.method !== "POST") {
-    return NextResponse.json(
-      { message: "Method not allowed" },
-      { status: 405 }
-    );
-  }
+// export async function POST(req, res) {
+//   if (req.method !== "POST") {
+//     return NextResponse.json(
+//       { message: "Method not allowed" },
+//       { status: 405 }
+//     );
 
-  try {
-    const userData = await req.json();
-    console.log(userData.email);
-    const user = await prisma.users.findFirst({
-      where: { email: userData.email },
-    });
+//   try {
+//     const userData = await req.json();
+//     console.log(userData.email);
+//     const user = await prisma.users.findFirst({
+//       where: { email: userData.email },
+//     });
 
-    if (!user) {
-      return NextResponse.json({ errors: ["User not found"] }, { status: 404 });
-    }
+//     if (!user) {
+//       return NextResponse.json({ errors: ["User not found"] }, { status: 404 });
+//     }
 
-    const tokenData = {
-      id: user.id.toString(),
-      email: user.email,
-      username: user.username,
-      picture: user.picture,
-      socials: user.socials,
-      bio: user.bio,
-      role: user.role,
-    };
+//     const tokenData = {
+//       id: user.id.toString(),
+//       email: user.email,
+//       username: user.username,
+//       picture: user.picture,
+//       socials: user.socials,
+//       bio: user.bio,
+//       role: user.role,
+//     };
 
-    // Generate a JWT token
-    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
-      expiresIn: "8h",
-    });
+//     // Generate a JWT token
+//     const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+//       expiresIn: "8h",
+//     });
 
-    const response = NextResponse.json(
-      {
-        id: user.id.toString(),
-        username: user.username,
-        email: user.email,
-        picture: user.picture,
-        socials: user.socials,
-        bio: user.bio,
-        role: user.role,
-      },
-      { status: 200 }
-    );
-    response.cookies.set("token", token, {
-      httpOnly: true,
-    });
+//     const response = NextResponse.json(
+//       {
+//         id: user.id.toString(),
+//         username: user.username,
+//         email: user.email,
+//         picture: user.picture,
+//         socials: user.socials,
+//         bio: user.bio,
+//         role: user.role,
+//       },
+//       { status: 200 }
+//     );
+//     response.cookies.set("token", token, {
+//       httpOnly: true,
+//     });
 
-    return response;
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        errors: ["Internal server error"],
-      },
-      { status: 500 }
-    );
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       {
+//         errors: ["Internal server error"],
+//       },
+//       { status: 500 }
+//     );
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
