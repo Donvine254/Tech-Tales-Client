640b4d4887148275c0998fed51390afeb0f1348e
Donvine <donvinemugendi@gmail.com>
2024-07-07 21:45:09 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,20 @@ export function middleware(request: NextRequest) {
     path.startsWith("/my-blogs") ||
     path.startsWith("/create");
 
+  const isPublicPath =
+    path.startsWith("/login") ||
+    path.startsWith("/register") ||
+    path.startsWith("/callback");
+
   const token = request.cookies.get("token");
   if (isProtectedPath && !token) {
     const redirectPath = path.slice(1);
     return NextResponse.redirect(
       new URL(`/login?post_login_redirect_url=${redirectPath}`, request.nextUrl)
     );
+  } else if (token && isPublicPath) {
+    //prevent users from visiting login page if they are already logged in
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 }
 
