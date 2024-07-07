e224f617dbeca11365607bbd9de10774aed659f4
Donvine <donvinemugendi@gmail.com>
2024-07-07 21:37:58 +0300

    update github icon

--- app/(blog)/me/page.jsx
+++ app/(blog)/me/page.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Loader from "@/components/Loader";
 import parse from "html-react-parser";
-import { Clipboard, Facebook, NewTwitterIcon } from "@/assets";
+import { Clipboard, Facebook, GithubIcon, NewTwitterIcon } from "@/assets";
 import secureLocalStorage from "react-secure-storage";
 import SocialMediaModal from "@/components/SocialMediaModal";
 
@@ -225,18 +225,7 @@ export default function Profile() {
                     target="_blank"
                     className="flex items-center justify-center gap-2">
                     {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      className="stroke-white">
-                      <title>Github</title>
-                      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                      <path d="M9 18c-4.51 2-5-2-7-2" />
-                    </svg>
+                    <GithubIcon />
                   </a>
                 ) : (
                   <span
@@ -245,18 +234,7 @@ export default function Profile() {
                       showModal("github");
                     }}>
                     {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      className="group-hover:stroke-white">
-                      <title>Github</title>
-                      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                      <path d="M9 18c-4.51 2-5-2-7-2" />
-                    </svg>
+                    <GithubIcon />
                     Connect Github
                   </span>
                 )}
