90befbe312ffc692519ca5eab53bf1232743306d
Donvine <donvinemugendi@gmail.com>
2024-07-21 23:19:34 +0300

    update explore route

--- app/(blog)/explore/[handle]/page.jsx
+++ app/(blog)/explore/[handle]/page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Explore from "./blogs";
 import prisma from "@/prisma/prisma";
+import { baseUrl } from "@/lib";
+import { Axios } from "axios";
 export const revalidate = 600;
 export const metadata = {
   title: "Explore Author Blogs - Tech Tales",
@@ -31,51 +33,10 @@ export async function generateStaticParams() {
   }
 }
 
-async function getUserBlogs(handle) {
-  "use server";
-  try {
-    // Fetch all users
-    const user = await prisma.user.findUnique({
-      where: {
-        handle: handle,
-      },
-    });
-
-    if (!user) {
-      throw new Error("User not found.");
-    }
-
-    // Use user.id to find blogs where authorId matches user.id
-    const blogs = await prisma.blog.findMany({
-      where: {
-        authorId: user.id,
-        status: "PUBLISHED",
-      },
-      include: {
-        author: {
-          select: {
-            username: true,
-            picture: true,
-          },
-        },
-        _count: {
-          select: {
-            comments: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-  } catch (error) {
-    console.error("Error fetching user blogs:", error);
-    throw error;
-  }
-}
-
 export default async function page({ params }) {
-  const blogs = await getUserBlogs(params.handle);
+  const blogs = await Axios.post(`${baseUrl}/blogs/explore`, {
+    handle: params.handle,
+  });
 
   return (
     <section className="md:mt-10">
--- app/api/blogs/explore/route.ts
+++ app/api/blogs/explore/route.ts
@@ -4,19 +4,37 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest, res: NextResponse) {
   const { handle } = await req.json();
 
-  const user = await prisma.findUnique({
-    where: {
-      handle: handle,
-      status: "ACTIVE",
-    },
-  });
+  if (!handle) {
+    return NextResponse.json(
+      { error: "Kindly provide a handle first" },
+      { status: 404 }
+    );
+  }
 
-  if (user) {
+  let user;
+
+  try {
+    user = await prisma.user.findUnique({
+      where: {
+        handle: handle,
+        status: "ACTIVE",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { error: "An error occurred while fetching the user." },
+      { status: 500 }
+    );
+  }
+
+  if (!user) {
     return NextResponse.json(
       { error: "No user with matching handle found" },
       { status: 404 }
     );
   }
+
   try {
     const blogs = await prisma.blog.findMany({
       where: {
