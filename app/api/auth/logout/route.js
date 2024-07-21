59092aed340206ef6e148450b85099f11fb80678
Donvine <donvinemugendi@gmail.com>
2024-07-22 01:43:25 +0300

    update signout route

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -10,10 +10,6 @@ export async function GET() {
       httpOnly: true,
       expires: new Date(0),
     });
-    response.cookies.set("isAdmin", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
     return response;
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
