57bb6ce69cdc0624b44f732bd7b2b77d7bde1ed4
Donvine <donvinemugendi@gmail.com>
2024-07-21 21:30:40 +0300

    update slug route

--- app/api/blogs/slug/route.ts
+++ app/api/blogs/slug/route.ts
@@ -2,44 +2,59 @@ import prisma from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { slug } = await req.json();
-  if (!slug) {
+  try {
+    const { slug } = await req.json();
+    if (!slug) {
+      return NextResponse.json(
+        { error: "Kindly provide the blog slug" },
+        { status: 404 }
+      );
+    } else {
+      const blog = await prisma.blog.findUnique({
+        where: {
+          slug: slug,
+        },
+        include: {
+          author: {
+            select: {
+              username: true,
+              picture: true,
+              bio: true,
+              socialMedia: {
+                select: {
+                  platform: true,
+                  handle: true,
+                },
+              },
+            },
+          },
+          comments: {
+            include: {
+              author: {
+                select: {
+                  username: true,
+                  picture: true,
+                  role: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      if (!blog) {
+        return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+      }
+
+      return NextResponse.json(blog, { status: 200 });
+    }
+  } catch (error) {
+    console.error(error);
     return NextResponse.json(
-      { error: "Kindly provide the blog slug" },
-      { status: 404 }
+      { error: "An error occurred while fetching the blog" },
+      { status: 500 }
     );
-  } else {
-    const blog = await prisma.blog.findUnique({
-      where: {
-        slug: slug,
-      },
-      include: {
-        author: {
-          select: {
-            username: true,
-            picture: true,
-            bio: true,
-            socialMedia: {
-              select: {
-                platform: true,
-                handle: true,
-              },
-            },
-          },
-        },
-        comments: {
-          include: {
-            author: {
-              select: {
-                username: true,
-                picture: true,
-                role: true,
-              },
-            },
-          },
-        },
-      },
-    });
-    return NextResponse.json(blog, { status: 200 });
+  } finally {
+    await prisma.$disconnect();
   }
 }
