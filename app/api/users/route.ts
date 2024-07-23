6bb162a3954bb52323ba0f8379deaef948506d87
Donvine <donvinemugendi@gmail.com>
2024-07-23 05:25:29 +0300

    update create route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,14 +1,8 @@
 import bcrypt from "bcryptjs";
 import prisma from "@/prisma/prisma";
+import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
-
-const createUserHandle = (username: string) => {
-  return username.replace(/\s+/g, "").toLowerCase();
-};
-
-const createUserAvatar = (username: string) => {
-  return `https://ui-avatars.com/api/?background=random&name=${username}`;
-};
+import { convertToHandle, createUserAvatar } from "@/lib/utils";
 
 const hashPassword = async (password: string) => {
   return await bcrypt.hash(password, 10);
@@ -21,10 +15,15 @@ function validateEmail(email: string) {
 type UserData = {
   username: string;
   email: string;
+  picture: string;
+  handle: string;
+  password_digest: string;
+};
+type RequestData = {
+  username: string;
+  email: string;
   password: string;
-  picture?: string;
   role?: string;
-  handle?: string;
 };
 
 export async function GET() {
@@ -57,9 +56,14 @@ export async function GET() {
 }
 
 //function to create new users
+//ensure emails and usernames are saved as lowercase letters. Admin cannot use this route when creating new users
 export async function POST(req: NextRequest, res: NextResponse) {
-  const requestData = (await req.json()) as UserData;
+  const requestData = (await req.json()) as RequestData;
   const isValidEmail = validateEmail(requestData.email);
+
+  const email = requestData.email.toLowerCase();
+  const handle = convertToHandle(requestData.username);
+
   if (!isValidEmail) {
     return NextResponse.json(
       { error: "Invalid email address" },
@@ -68,40 +72,74 @@ export async function POST(req: NextRequest, res: NextResponse) {
   }
   const existingUser = await prisma.user.findFirst({
     where: {
-      OR: [{ email: requestData.email }, { username: requestData.username }],
+      OR: [{ email: email }, { handle: handle }],
     },
     select: {
       email: true,
-      username: true,
+      handle: true,
     },
   });
 
   if (existingUser) {
-    if (
-      existingUser.email === requestData.email &&
-      existingUser.username === requestData.username
-    ) {
+    const normalizedEmail = existingUser.email.toLowerCase();
+    if (normalizedEmail === email && existingUser.handle === handle) {
       return NextResponse.json(
         { error: "Both email and username are already taken" },
         { status: 422 }
       );
-    } else if (existingUser.email === requestData.email) {
+    } else if (normalizedEmail === email) {
       return NextResponse.json(
         { error: "Email is already taken" },
         { status: 422 }
       );
-    } else if (existingUser.username === requestData.username) {
+    } else if (existingUser.handle === handle) {
       return NextResponse.json(
         { error: "Username is already taken" },
         { status: 422 }
       );
     }
   }
-  const data = {
-    ...requestData,
-    avatar: createUserAvatar(requestData.username),
-    handle: createUserHandle(requestData.username),
+  const data: UserData = {
+    username: requestData.username.toLowerCase(),
+    picture: createUserAvatar(requestData.username),
+    email: email,
+    handle: handle,
     password_digest: await hashPassword(requestData.password),
   };
-  return NextResponse.json(data);
+  try {
+    const user = await prisma.user.create({
+      data: data,
+      include: {
+        socialMedia: {
+          select: {
+            platform: true,
+            handle: true,
+          },
+        },
+      },
+    });
+    const tokenData = user;
+    // Generate a JWT token
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "8h",
+    });
+
+    // Return user details and token
+    const response = NextResponse.json(
+      user,
+
+      { status: 200 }
+    );
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("something went wrong", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
