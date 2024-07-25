b456ff02094bd8557ee9e404d123610e31af00bf
Donvine <donvinemugendi@gmail.com>
2024-07-25 03:24:31 +0300

    update login route for optimization

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,59 +1,83 @@
-import prisma from "@/prisma/prisma";
 import bcrypt from "bcryptjs";
+import prisma from "@/prisma/prisma";
 import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 
+const rateLimitMap = new Map();
+
 export async function POST(req: NextRequest, res: NextResponse) {
   const { email, password } = await req.json();
-  const suspensionMs = 5 * 60 * 1000;
+  const ip = req.ip || req.headers.get("X-Forwarded-For");
+  const limit = 3; // Limiting requests to 3 login attempts per minute per IP
+  const windowMs = 60 * 1000; // 1 minute
+  const suspensionMs = 5 * 60 * 1000; // 5 minutes
+
+  // Function to limit requests to 3 login attempts per IP
   const now = Date.now();
+  if (!rateLimitMap.has(ip)) {
+    rateLimitMap.set(ip, { count: 0, lastReset: now, suspendedUntil: null });
+  }
+
+  const ipData = rateLimitMap.get(ip);
+
+  // Check if the IP is currently suspended
+  if (ipData.suspendedUntil && now < ipData.suspendedUntil) {
+    return NextResponse.json(
+      { error: "Too Many Requests. Try again after 5 minutes" },
+      { status: 429 }
+    );
+  }
+
+  if (now - ipData.lastReset > windowMs) {
+    ipData.count = 0;
+    ipData.lastReset = now;
+  }
+
+  if (ipData.count >= limit) {
+    ipData.suspendedUntil = now + suspensionMs;
+    return NextResponse.json(
+      { error: "Too Many Requests. Try again after 5 minutes" },
+      { status: 429 }
+    );
+  }
+
+  ipData.count += 1;
+
   try {
     // Check if the user exists
     const user = await prisma.user.findUnique({
-      where: { email: email.toLowerCase(), deleted: false },
+      where: {
+        email: email.toLowerCase(),
+        deleted: false,
+      },
     });
-    if (!user) {
-      return NextResponse.json(
-        { error: "No user with matching email found" },
-        { status: 404 }
-      );
-    }
-    //run this only if user status is not Inactive
-    if (user.status !== "INACTIVE" && user.status !== "ACTIVE") {
-      if (user.status === "DEACTIVATED") {
-        const deactivatedAt = new Date(user.deactivatedAt);
-        const timeDiff = now - deactivatedAt.getTime();
-        const remainingMinutes = Math.ceil((suspensionMs - timeDiff) / 60000);
-        if (remainingMinutes > 0) {
-          return NextResponse.json(
-            {
-              error: `This user account has been ${user.status.toLowerCase()}. Please try again in ${remainingMinutes} minutes.`,
-            },
-            { status: 404 }
-          );
-        }
-      }
-      if (user.status === "SUSPENDED") {
+
+    // Compare the password
+
+    if (user) {
+      if (user.status === "SUSPENDED" || user.status === "DEACTIVATED") {
         return NextResponse.json(
           {
-            error:
-              "This user account has been suspended. Please contact your administrator",
+            error: `This user account has been ${user.status.toLowerCase()}. Please contact your administrator`,
           },
           { status: 404 }
         );
+      } else {
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          user.password_digest
+        );
+        if (!isPasswordValid) {
+          return NextResponse.json(
+            { error: "Invalid password, please try again!" },
+            { status: 401 }
+          );
+        }
       }
-    }
-
-    // Compare the password
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      user.password_digest
-    );
-
-    if (!isPasswordValid) {
+    } else {
       return NextResponse.json(
-        { error: "Invalid password, please try again!" },
-        { status: 401 }
+        { error: "No user with matching email found" },
+        { status: 404 }
       );
     }
 
