9f5e0648fc3b2ccaccfa05f3ce8c0a4a7f80950b
Donvine <donvinemugendi@gmail.com>
2024-08-03 13:15:59 +0300

    add suspense boundary to reset page

--- app/(login)/reset/page.jsx
+++ app/(login)/reset/page.jsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import ResetPage from "./reset";
+import ResetMainPage from "@/components/reset/reset";
 
 export default function Page() {
-  return <ResetPage />;
+  return (
+    <Suspense fallback={<ResetMainPage />}>
+      <ResetPage />
+    </Suspense>
+  );
 }
