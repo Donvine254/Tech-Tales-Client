83b734a6f7f02e38228e6cf1f0188e633bf7d097
Donvine <donvinemugendi@gmail.com>
2024-08-05 23:50:25 +0300

    update user profile image

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,10 +3,7 @@ export function UserImage({ url, className, size = 48 }) {
   return (
     <Image
       className={`h-10 w-10 md:h-12 md:w-12 rounded-full cursor-pointer ${className}`}
-      src={
-        url ??
-        "https://res.cloudinary.com/dipkbpinx/image/upload/v1705280157/gztaho1v2leujxr8w3c8.png"
-      }
+      src={url ?? "/logo.png"}
       width={size}
       height={size}
       title="User Profile Photo"
