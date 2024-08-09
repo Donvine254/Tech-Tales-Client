24ea5ef7f102cef96017164d81f48e4ec0a74614
Donvine <donvinemugendi@gmail.com>
2024-08-09 05:01:03 +0300

    update mailer function

--- app/api/mailer/test/route.ts
+++ app/api/mailer/test/route.ts
@@ -1,10 +1,10 @@
-import { sendVerificationEmail } from "@/emails";
+import { sendVerificationEmail, sendWelcomeEmail } from "@/emails";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { email, code } = await req.json();
+  const { email, name } = await req.json();
   try {
-    const response = await sendVerificationEmail(email, code);
+    const response = await sendWelcomeEmail(email, name);
     console.log(response);
     return NextResponse.json(response);
   } catch (error) {
--- emails/template.ts
+++ emails/template.ts
@@ -14,17 +14,17 @@ export const otpTemplate = (otpCode: string) => `
   </div>
 `;
 export const welcomeTemplate = (name: string) => `
-  <div>
-    <h3>Welcome to Techtales!</h3>
+  <div style="width:100%; padding:2px; background-color: #F7F7F7">
+  <img src="https://www.touchpoint.com/wp-content/uploads/2023/11/Featured-image-1.png" style="width: 100%; max-height: 360px; margin:auto;"/>
+    <h3 style="text-align: center;color: #3B82F6">Welcome to Techtales!</h3>
     <p> Dear ${name} </p>
-    <p>Thank you for joining TechTales! We're excited to have you on board. Explore, learn, and share your tech stories with us. We encourage you to adhere to our <a href="https://techtales.vercel.app/terms">terms and conditions </a>and follow our <a href="https://techtales.vercel.app/community">community guidelines </a> when engaging with others.</p>
-    <footer style="
-        font-size: 12px; 
-        color: #888; 
-        padding-top: 10px; 
-        font-style: italic;">
+    <p>Thank you for joining TechTales! We're excited to have you on board.</p>
+    <p>We are a community of developers and tech enthusiasts where you can explore, learn, and share your tech stories.</p>
+    <p>We encourage you to adhere to our <a href="https://techtales.vercel.app/terms">terms and conditions </a>and follow our <a href="https://techtales.vercel.app/community">community guidelines </a> when engaging with others.</p>
+    <footer style="font-size: 10px; color:#06b6d4; background-color: #cffafe; padding: 5px;border-radius: 5px; border: 1px solid #06b6d4;font-style: italic;">
       This is an automated message, please do not reply to this email
     </footer>
+    
   </div>
 `;
 
@@ -40,6 +40,7 @@ export const adminRegistrationTemplate = (
           <div style="background-color: #f0f0f0; color: #333; padding: 5px; border-radius: 5px;"> <p>Email: <strong>${email}</strong></p>
           <p>Password: <strong>${password}</strong></p></div>
           <p>Please log in and change your password as soon as possible.</p>
+          <hr></hr>
           <p>We encourage you to adhere to our <a href="https://techtales.vercel.app/terms">terms and conditions </a>and follow our <a href="https://techtales.vercel.app/community">community guidelines </a> when engaging with others.</p>
           <footer style="
         font-size: 12px; 
