146b16a2037f28ea9feef3fcdb3b3e2581237b76
Donvine <donvinemugendi@gmail.com>
2024-08-16 03:27:45 +0300

    add regenerate summary button

--- components/blogSummary.jsx
+++ components/blogSummary.jsx
@@ -56,9 +56,10 @@ export default function BlogSummary({ body, show, id }) {
                 width="20">
                 <path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 10112 0 56 56 0 10-112 0zm0 284a56 56 0 10112 0 56 56 0 10-112 0zm0 284a56 56 0 10112 0 56 56 0 10-112 0z" />
               </svg>
-              <h1 className="">SUMMARY</h1>
+
+              <h1>SUMMARY</h1>
             </div>
-            <ul className="list-disc pl-4 text-base xsm:text-[12px] font-extralight">
+            <ul className="summary  text-base xsm:text-[12px] font-extralight">
               {parse(summary)}
             </ul>
           </div>
@@ -71,9 +72,17 @@ export default function BlogSummary({ body, show, id }) {
               <path d="M8 15A7 7 0 118 1a7 7 0 010 14zm0 1A8 8 0 108 0a8 8 0 000 16z" />
               <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 11-2 0 1 1 0 012 0z" />
             </svg>
-            <small className="flex-1">
+            <small className="flex-1 flex-col  xsm:text-xs">
               This summary is AI generated and we cannot guarantee it&apos;s
               accuracy. We encourage you to read the full blog post.
+              <button
+                className="bg-green-500 rounded-md px-0.5 font-medium hover:bg-green-600 text-white"
+                onClick={getBlogSummary}
+                title="regenerate blog summary"
+                disabled={loading}>
+                {" "}
+                {loading ? "✨Generating..." : " ↻ Regenerate"}
+              </button>
             </small>
           </div>
         </div>
@@ -83,7 +92,9 @@ export default function BlogSummary({ body, show, id }) {
             <div className="mx-auto sm:w-1/2 md:w-1/3 p-4">
               <button
                 className="py-1 w-full bg-gradient-to-r from-blue-400 via-green-400 to-purple-400 shadow shadow-purple-400 text-white relative rounded-full hover:shadow-lg hover:shadow-purple-400 hover:-translate-y-1 transition-transform duration-300"
-                onClick={getBlogSummary}>
+                onClick={getBlogSummary}
+                disabled={loading}
+                title="generate blog summary">
                 {loading ? (
                   <span className="flex items-center justify-center">
                     <span className="animate-spin mr-2">✨</span> Generating...
