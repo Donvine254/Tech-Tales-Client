d52aed7ab65a094555a564604e2c5a100b63deb3
Donvine <donvinemugendi@gmail.com>
2024-08-20 05:12:01 +0300

    update prism to highlight all langauges

--- components/ui/prismLoader.jsx
+++ components/ui/prismLoader.jsx
@@ -1,9 +1,19 @@
 "use client";
-
 import { useEffect } from "react";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
+import "prismjs/components/prism-markup-templating.js";
 import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-php";
+import "prismjs/components/prism-haml";
+import "prismjs/components/prism-markup";
 
 export default function PrismLoader() {
   useEffect(() => {
