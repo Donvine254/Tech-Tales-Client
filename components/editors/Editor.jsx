8dab3c0de552ce5cc7d5bd9b16dd5270eb66c390
Donvine <donvinemugendi@gmail.com>
2024-08-22 05:44:26 +0300

    update code styling

--- components/editors/Editor.jsx
+++ components/editors/Editor.jsx
@@ -74,7 +74,7 @@ export default function App({ data, handleChange, onFocus }) {
             "accordion",
           ],
           toolbar:
-            "undo redo | blocks | fontfamily | bold italic forecolor underline| align numlist bullist | link image table media | backcolor  emoticons codesample blockquote| preview removeformat",
+            "undo redo | blocks | fontfamily | bold italic forecolor underline| align numlist bullist | link image table media | backcolor  emoticons codesample code blockquote| preview removeformat",
           content_style:
             "@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap'); body { font-family: 'Segoe UI'; height: 'auto'; overflow: 'hidden'; }",
           image_advtab: true,
