c3ec3de3d35976ef5255ef61f50887816e2ff641
Donvine <donvinemugendi@gmail.com>
2024-08-24 02:46:20 +0300

    update combo box to use state

--- components/Search.jsx
+++ components/Search.jsx
@@ -2,17 +2,17 @@
 import { useState, useEffect, useRef } from "react";
 import { SearchIcon, SortDown } from "@/assets";
 import { useRouter } from "next/navigation";
-import { categories } from "@/constants";
+import { categories, options } from "@/constants";
 import { Tooltip } from "react-tooltip";
-import Script from "next/script";
 
 export const Search = () => {
   const router = useRouter();
-  const [query, setQuery] = useState("");
   const [isListening, setIsListening] = useState(false);
   const [category, setCategory] = useState("");
   const [showDropdown, setShowDropdown] = useState(false);
   const [showDropdownOptions, setShowDropdownOptions] = useState(false);
+  const [comboOptions, setComboOptions] = useState(options);
+  const [showComboOptions, setShowComboOptions] = useState(false);
   const dropdownRef = useRef(null);
   useEffect(() => {
     if (isListening) {
@@ -22,7 +22,8 @@ export const Search = () => {
 
       recognition.onresult = (event) => {
         const transcript = event.results[0][0].transcript;
-        setQuery(transcript);
+        const input = document.getElementById("combobox-input");
+        input.value = transcript.trim();
         setIsListening(false);
         router.replace(`/search?search=${transcript.trim()}`);
       };
@@ -48,11 +49,20 @@ export const Search = () => {
     };
   }, []);
 
-  // function handleSearch(e) {
-  //   e.preventDefault();
-  //   const value = query.trim();
-  //   router.replace(`/search?search=${value}`);
-  // }
+  function handleSearch(e) {
+    const value = e.target.value.toLowerCase();
+    setComboOptions("");
+    const filteredOptions = options.filter((option) =>
+      option.toLowerCase().includes(value)
+    );
+    if (filteredOptions.length > 0) {
+      setShowComboOptions(true);
+      setComboOptions(filteredOptions);
+    } else {
+      setComboOptions(["No Results Found"]);
+    }
+  }
+
   function startVoiceSearch() {
     setIsListening(true);
   }
@@ -67,9 +77,15 @@ export const Search = () => {
     }, 100);
   }
 
+  const handleComboSearch = (option) => {
+    const input = document.getElementById("combobox-input");
+    input.value = option.trim();
+    input.focus();
+    setShowComboOptions(false);
+  };
+
   return (
     <div className="hidden md:flex  md:items-center gap-2">
-      <Script src="script.js" type="module"></Script>
       <form
         className="md:py-2 md:flex md:items-center md:justify-center"
         action="/search/">
@@ -79,6 +95,7 @@ export const Search = () => {
             id="combobox-input"
             name="search"
             minLength={2}
+            onInput={handleSearch}
             placeholder="Search blogs..."
             autoCorrect="on"
             spellCheck={true}
@@ -93,7 +110,7 @@ export const Search = () => {
             width="1.25rem"
             className="w-5 h-5 text-gray-900 absolute right-1 top-1/2 transform -translate-y-1/2 hover:text-cyan-500 focus:outline-none"
             onClick={startVoiceSearch}
-            aria-hidden="true"
+            aria-description="search by voice"
             data-tooltip-id="voice-search">
             <path
               d="M3.5 6.5A.5.5 0 014 7v1a4 4 0 008 0V7a.5.5 0 011 0v1a5 5 0 01-4.5 4.975V15h3a.5.5 0 010 1h-7a.5.5 0 010-1h3v-2.025A5 5 0 013 8V7a.5.5 0 01.5-.5z"
@@ -111,7 +128,25 @@ export const Search = () => {
             style={{ padding: "4px" }}
           />
           <SearchIcon className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
-          <div className="options" id="options-container"></div>
+          {showComboOptions && (
+            <div
+              className="absolute top-full left-0 right-0 border border-gray-300 max-h-[250px] max-w-[400px] mx-auto mt-2 overflow-y-auto bg-white block rounded-lg z-50"
+              id="options-container">
+              {comboOptions &&
+                comboOptions?.map((option, index) => (
+                  <div
+                    key={index}
+                    className={`${
+                      option === "No Results Found"
+                        ? "p-2 text-[#999]"
+                        : "p-2 cursor-pointer hover:bg-[##f0f0f0]"
+                    }`}
+                    onClick={() => handleComboSearch(option)}>
+                    {option}
+                  </div>
+                ))}
+            </div>
+          )}
         </search>
         <div
           className="cursor-pointer border-2 bg-gray-50 hover:bg-cyan-500 hover:text-slate-200 rounded-xl p-2 px-3 m-1 border-gray-300 hover:border-cyan-500 shadow "
