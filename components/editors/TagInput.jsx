d290c8eb46de1ee7cba5eff8628352913f1a813a
Donvine <donvinemugendi@gmail.com>
2024-08-24 07:43:58 +0300

    updated ai button

--- components/editors/TagInput.jsx
+++ components/editors/TagInput.jsx
@@ -58,6 +58,8 @@ const TagInput = ({ setBlogData, blogTags, title }) => {
       setShowAIButton(false);
       return false;
     }
+    const button = document.getElementById("generateTags");
+    button.classList.add("animate-spin");
     setBlogData((prev) => ({
       ...prev,
       tags: "",
@@ -76,6 +78,7 @@ const TagInput = ({ setBlogData, blogTags, title }) => {
     }));
     toast.success("Tags generated successfully");
     setShowAIButton(false);
+    button.classList.remove("animate-spin");
   }
 
   return (
@@ -101,7 +104,9 @@ const TagInput = ({ setBlogData, blogTags, title }) => {
         onKeyDown={handleKeyDown}
         onInput={() => setShowAIButton(false)}
         maxLength={15}
-        onFocus={() => setShowAIButton(true)}
+        onFocus={() => {
+          if (title) setShowAIButton(true);
+        }}
         disabled={blogTags.length >= 4}
         placeholder={
           blogTags.length >= 4
@@ -114,12 +119,12 @@ const TagInput = ({ setBlogData, blogTags, title }) => {
       />
       {showAIButton && title && (
         <button
-          className="flex items-center gap-2  text-sm text-white bg-gradient-to-r from-blue-400 via-green-400 to-purple-400 shadow-2xl shadow-purple-400 rounded-md px-1 absolute right-5 "
+          id="generateTags"
+          className="text-sm   absolute right-5 hover:bg-gray-900 px-1 py-0.5 rounded-md "
           type="button"
           onClick={generateTags}
           data-tooltip-id="generate-tags">
-          <span className=" font-bold ">AI</span>
-          <svg viewBox="0 0 576 512" fill="currentColor" height="14" width="14">
+          <svg viewBox="0 0 576 512" fill="#FF822D" height="14" width="14">
             <path d="M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1 14.1 37.7c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2l-37.7-14.1L263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5l-14.1 37.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0l381.3-381.4c18.7-18.7 18.7-49.1 0-67.9l-34.6-34.5c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z" />
           </svg>
           <Tooltip
