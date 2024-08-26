bf4cc09eacecdf951fec9d1516641a8c7dd1d6fd
Donvine <donvinemugendi@gmail.com>
2024-08-26 23:32:37 +0300

    update search functionality to include blog titles

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -5,7 +5,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
   const { search } = await req.json();
 
   try {
-    const blogs = await prisma.blog.findMany({
+    // First, try to find blogs by tag
+    let blogs = await prisma.blog.findMany({
       where: {
         status: "PUBLISHED",
         tags: {
@@ -26,17 +27,45 @@ export async function POST(req: NextRequest, res: NextResponse) {
           },
         },
       },
-
       orderBy: {
         createdAt: "desc",
       },
       cacheStrategy: { ttl: 60 },
     });
 
+    // If no blogs are found by tag, search by title
+    if (blogs.length === 0) {
+      blogs = await prisma.blog.findMany({
+        where: {
+          status: "PUBLISHED",
+          title: {
+            contains: search.toString(),
+            mode: "insensitive",
+          },
+        },
+        include: {
+          author: {
+            select: {
+              username: true,
+              picture: true,
+            },
+          },
+          _count: {
+            select: {
+              comments: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        cacheStrategy: { ttl: 60 },
+      });
+    }
+
     return NextResponse.json(blogs, { status: 200 });
   } catch (error) {
     console.error("Error fetching blogs:", error);
-
     return NextResponse.json(
       { error: "An error occurred while fetching blogs." },
       { status: 500 }
