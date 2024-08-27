c906fddb318a953e21dca90d37a23c35f458a030
Donvine <donvinemugendi@gmail.com>
2024-08-27 21:42:14 +0300

    update recommendations route

--- app/api/recommendations/route.ts
+++ app/api/recommendations/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { tags } = await req.json();
+  const { tags, blogId } = await req.json();
   const tagsArray = tags.split(",").map((tag) => tag.trim());
   try {
     const blogs = await prisma.blog.findMany({
@@ -10,12 +10,27 @@ export async function POST(req: NextRequest, res: NextResponse) {
         OR: tagsArray.map((tag) => ({
           tags: {
             contains: tag,
+            mode: "insensitive",
           },
         })),
       },
-      take: 5,
+      include: {
+        author: {
+          select: {
+            username: true,
+            picture: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+      take: 6,
     });
-    return NextResponse.json(blogs);
+    const filteredBlogs = blogs.filter((blog) => blog.id !== Number(blogId));
+    return NextResponse.json(filteredBlogs);
   } catch (error) {
     console.error("Error fetching blogs:", error);
     return NextResponse.json(
