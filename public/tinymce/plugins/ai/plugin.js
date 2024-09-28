f11856596b3a170b1cd19650a8a30b9d19f463bf
Donvine <donvinemugendi@gmail.com>
2024-09-28 17:15:38 +0300

    update ai plugin

--- public/tinymce/plugins/ai/plugin.js
+++ public/tinymce/plugins/ai/plugin.js
@@ -37,7 +37,7 @@ tinymce.PluginManager.add("ai", function (editor, url) {
         }
         const result = document.getElementById("ai-response-container");
         result.innerHTML += `<div style="background-color: #f1f2f3; padding: 10px; border-radius: 5px;display:flex; justify-content:center; align-items:center;flex-direction: column;"><svg xmlns="http://www.w3.org/2000/svg" style=" margin: auto;background: none;display: block; shape-rendering: auto;"width="50px"height="50px"viewBox="0 0 100 100"preserveAspectRatio="xMidYMid"><circle cx="50"cy="50"fill="none" stroke="#888"stroke-width="8"r="35" stroke-dasharray="164.93361431346415 56.97787143782138"><animateTransform attributeName="transform"type="rotate" repeatCount="indefinite"dur="1s"values="0 50 50;360 50 50"keyTimes="0;1"></animateTransform></circle> </svg><p style="text-align: center">Generating....</p></div>`;
-        fetch("http://localhost:3000/api/gemini", {
+        fetch("https://techtales.vercel.app/api/gemini", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
