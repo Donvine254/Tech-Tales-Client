5b99c8189eee56766e3bc9551fc08450f67feb16
Donvine <donvinemugendi@gmail.com>
2024-10-02 07:22:32 +0300

    add import word plugin

--- components/editors/Editor.jsx
+++ components/editors/Editor.jsx
@@ -31,6 +31,10 @@ export default function App({ data, handleChange, onFocus }) {
         crossorigin="anonymous"
         referrerpolicy="no-referrer"
         async></script>
+      <script
+        src="https://unpkg.com/mammoth/mammoth.browser.min.js"
+        type="text/javascript"
+        async></script>
       <Editor
         tinymceScriptSrc="/tinymce/tinymce.min.js"
         licenseKey="gpl"
@@ -91,11 +95,15 @@ export default function App({ data, handleChange, onFocus }) {
             "ai",
             "quickbars",
             "pageembed",
+            "importword",
           ],
           toolbar:
-            "undo redo | blocks | fontfamily | bold italic forecolor underline| align numlist bullist indent| link image table media pageembed| backcolor  emoticons codesample blockquote| ai code removeformat",
+            "undo redo| blocks | fontfamily | bold italic forecolor underline| align numlist bullist indent| link image table media pageembed| backcolor  emoticons codesample blockquote| ai code importword removeformat",
           menu: {
-            tools: { title: "Tools", items: "ai pageembed  wordcount" },
+            tools: {
+              title: "Tools",
+              items: "ai  importword  wordcount help",
+            },
           },
           content_style:
             "@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap'); body { font-family: 'Segoe UI'; height: 'auto'; overflow: 'hidden'; }",
