96ec9c93b3d85688a96f13f1046778263e042e12
Donvine <donvinemugendi@gmail.com>
2024-10-20 01:50:08 +0300

    update reading history styling

--- app/(blog)/me/history/history.jsx
+++ app/(blog)/me/history/history.jsx
@@ -38,6 +38,7 @@ export default function History() {
             body: JSON.stringify({ blogIds: historyArray }),
           });
           const data = await response.json();
+          console.log(data);
           setHistory(data);
           setLoading(false);
         } else {
@@ -54,16 +55,15 @@ export default function History() {
   }
 
   return (
-    <div className="w-full min-h-screen mx-auto px-4 md:px-8 md:w-2/3 relative font-poppins">
+    <div className="w-full min-h-screen mx-auto px-4 md:px-8 lg:w-2/3 relative font-poppins">
       <SideNav />
-      <div className="flex items-center gap-4">
-        <button className="px-6 py-1 font-bold my-2 bg-gray-300 hover:bg-blue-100 hover:text-blue-500 border hover:border-blue-500 rounded-md">
-          Reading History
-        </button>
-        <button className="px-6 py-1 font-bold my-2  bg-transparent hover:bg-blue-500 hover:text-white rounded-md">
+      <h3 className="px-6 py-1 font-bold my-2 bg-gray-300 hover:bg-blue-100 hover:text-blue-500 max-w-fit border hover:border-blue-500 rounded-md xsm:text-sm">
+        Reading History
+      </h3>
+      {/* <button className="px-6 py-1 font-bold my-2  bg-transparent hover:bg-blue-500 hover:text-white rounded-md xsm:text-sm">
           Search History
-        </button>
-      </div>
+        </button> */}
+
       <section>
         {loading && (
           <div>
@@ -78,85 +78,86 @@ export default function History() {
           history.map((blog) => (
             <div
               key={blog.id}
-              className="bg-gray-50 my-4 p-4 rounded-md border shadow hover:bg-[#fefefe]">
-              <div className="">
-                <div className="flex gap-2 xsm:items-center">
-                  <UserImage url={blog.author.picture} />
-                  <div className="">
-                    <p className="text-sm sm:text-base md:text-xl font-medium capitalize">
-                      {blog.author.username}
-                    </p>
-                    <p className="text-base xsm:text-xs xsm:mb-0">
-                      <span className="xsm:hidden">Published on </span>{" "}
-                      <time dateTime={blog?.createdAt}>
-                        {formatDate(blog.createdAt)} {""}
-                      </time>
-                      &#x2022; &#128337;{calculateReadingTime(blog.body)} min
-                    </p>
+              className="hover:bg-blue-100 p-1 border-b border-slate-300  ">
+              <div className="flex items-center gap-x-4 gap-y-1 py-2 flex-wrap md:flex-nowrap">
+                <a
+                  href={`/blogs/${blog.slug}`}
+                  rel="noopener noreferrer"
+                  className="relative">
+                  <img
+                    src={blog.image.secure_url}
+                    alt={blog.slug}
+                    loading="lazy"
+                    title="read blog"
+                    width={160}
+                    height={90}
+                    className="object-contain object-center xsm:w-full xsm:h-auto h-[90px] w-[160px] italic"
+                  />
+                  <img
+                    src={blog.author.picture}
+                    alt={blog.author.username}
+                    title={blog.author.username}
+                    height="24"
+                    width="24"
+                    className="rounded-full h-6 w-6 absolute left-0 right-0 mx-auto bottom-[-10px] cursor-pointer object-center align-middle ring-2 "
+                  />
+                </a>
+                <div className="flex-grow">
+                  <Link href={`/blogs/${blog.slug}`} prefetch>
+                    <h3 className="break-words xsm:text-xs">{blog.title}</h3>
+                  </Link>
+                  <div className="py-1">
+                    {blog.tags ? (
+                      <div className="flex gap-1 md:gap-2 flex-wrap text-sm xsm:text-xs">
+                        {blog.tags.split(",").map((tag, index) => (
+                          <Link
+                            key={index}
+                            href={`/search?search=${tag.trim()}`}
+                            className={` text-blue-500  highlight-tag-${index}`}>
+                            <span>#</span>
+                            {tag.trim()}
+                          </Link>
+                        ))}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                  <div className="flex items-center justify-between xsm:gap-2 md:gap-4 py-2 text-sm xsm:text-xs">
+                    <div className="flex justify-between gap-4">
+                      <p className="">
+                        {calculateReadingTime(blog.body)} min{" "}
+                        <span className="xsm:hidden">read</span>
+                      </p>
+                      <p>
+                        &#x2022; {formatViews(blog.views)}{" "}
+                        <span className="xsm:hidden">views</span>
+                      </p>
+                    </div>
+                    <div className="flex justify-end items-center gap-4">
+                      <ShareButton
+                        size={16}
+                        className="h-[16px] w-[16px] text-gray-500 hover:text-blue-500"
+                        title={blog.title}
+                        slug={blog.slug}
+                      />
+                      <Bookmark size={16} blogId={blog.id} />
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        height="24"
+                        width="24"
+                        className="text-gray-500 hover:text-red-500  rounded-full p-0.5 hover:bg-red-100">
+                        <title>Remove Post</title>
+                        <path
+                          fill="currentColor"
+                          d="M15.964 4.634h-12v2h12v-2zM15.964 8.634h-12v2h12v-2zM3.964 12.634h8v2h-8v-2zM12.964 13.71l1.415-1.415 2.121 2.121 2.121-2.12 1.415 1.413-2.122 2.122 2.122 2.12-1.415 1.415-2.121-2.121-2.121 2.121-1.415-1.414 2.122-2.122-2.122-2.12z"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
-                <hr className="mt-2 h-[2px] bg-gray-200" />
-                <Link
-                  href={`/blogs/${blog.slug}`}
-                  className="space-y-3 xl:col-span-3"
-                  prefetch>
-                  <h1 className="font-bold md:text-xl  py-2 ">{blog.title}</h1>
-                </Link>
-                {/* div for blog tags */}
-                <div className="py-1">
-                  {blog.tags ? (
-                    <div className="flex gap-1 md:gap-2 flex-wrap text-sm xsm:text-xs">
-                      {blog.tags.split(",").map((tag, index) => (
-                        <Link
-                          key={index}
-                          href={`/search?search=${tag.trim()}`}
-                          className={` text-blue-500  highlight-tag-${index}`}>
-                          <span>#</span>
-                          {tag.trim()}
-                        </Link>
-                      ))}
-                    </div>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-                <article className="text-sm sm:text-base md:text-[18px] leading-8 md:pb-1 line-clamp-2  overflow-hidden trimmed-blog-body ">
-                  {blog ? parse(blog.body.substring(0, 400)) : blog.body}
-                </article>
               </div>
-              {/* start of flex div for actions */}
-              <div className="flex items-center justify-between xsm:gap-2 md:gap-4 py-2 ">
-                <Link
-                  href={`/blogs/${blog.slug}`}
-                  className="text-base xsm:text-xs inline-flex items-center gap-x-1 ">
-                  <Comment size={20} className="stroke-none fill-gray-400" />
-                  <span>{blog?._count?.comments}</span>
-                </Link>
-                <Link
-                  href={`/blogs/${blog.slug}`}
-                  prefetch
-                  className="inline-flex items-center gap-x-0.5  ">
-                  <Like className="stroke-gray-400 fill-none" size={20} />
-                  <span className="text-base xsm:text-xs">{blog.likes}</span>
-                </Link>
-                <Link
-                  href={`/blogs/${blog.slug}`}
-                  prefetch
-                  className="inline-flex xsm:items-center  sm:items-start gap-x-0.5 ">
-                  <Graph className="stroke-gray-500 fill-none " size={20} />
-                  <p className="text-base xsm:text-xs sm:align-text-bottom  xsm:pt-1.5">
-                    {formatViews(blog.views)}
-                  </p>
-                </Link>
-                <ShareButton
-                  size={20}
-                  className="h-[20px] w-[20px] text-gray-500"
-                  title={blog.title}
-                  slug={blog.slug}
-                />
-                <Bookmark />
-              </div>
-              {/* end of flex div */}
             </div>
           ))
         ) : (
