af216d6934a9f9cde60d4fa75167e07906b62c29
Donvine <donvinemugendi@gmail.com>
2024-11-03 05:41:33 +0300

    update response buttons

--- components/blogs/Responses.jsx
+++ components/blogs/Responses.jsx
@@ -10,35 +10,33 @@ import {
   ReportAbuseBtn,
 } from "../ui/Buttons";
 export default function Response({ response, key, index, user, blogAuthorId }) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
   return (
     <div
-      className={`flex items-start gap-4 text-sm xsm:text-xs my-1 p-2 xsm:p-2 xsm:gap-2 ${
+      className={`flex items-start gap-4 text-sm xsm:text-xs my-1 p-2 xsm:p-2 xsm:gap-2 relative ${
         index === 0 ? "border-l-2  border-gray-400" : ""
       }`}
       key={key}>
       {/* User Picture */}
       {isExpanded ? (
         <>
-          <div className="flex flex-col gap-2 items-center">
-            <Image
-              src={response.author.picture}
-              alt={response.author.username}
-              className="w-8 h-8 rounded-full ring-2 ring-offset-2 ring-pink-600 self-start response-avatar"
-              height="32"
-              width="32"
-            />
-            <svg
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              height="1em"
-              width="1em"
-              onClick={() => setIsExpanded(false)}
-              className="cursor-pointer  rounded-full">
-              <title>Hide Reply</title>
-              <path d="M12 19.24l-4.95-4.95-1.41 1.42L12 22.07l6.36-6.36-1.41-1.42L12 19.24zM5.64 8.29l1.41 1.42L12 4.76l4.95 4.95 1.41-1.42L12 1.93 5.64 8.29z" />
-            </svg>
-          </div>
+          <Image
+            src={response.author.picture}
+            alt={response.author.username}
+            className="w-8 h-8 xsm:w-6 xsm:h-6 rounded-full ring-2 ring-offset-2 ring-pink-600 self-start response-avatar"
+            height="32"
+            width="32"
+          />
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="1em"
+            width="1em"
+            onClick={() => setIsExpanded(false)}
+            className="cursor-pointer  rounded-full absolute top-0 bottom-0 left-4 my-auto">
+            <title>Hide Reply</title>
+            <path d="M12 19.24l-4.95-4.95-1.41 1.42L12 22.07l6.36-6.36-1.41-1.42L12 19.24zM5.64 8.29l1.41 1.42L12 4.76l4.95 4.95 1.41-1.42L12 1.93 5.64 8.29z" />
+          </svg>
           <div>
             <div className="flex flex-col bg-zinc-100 rounded-lg text-sm  border shadow p-3">
               <div className="flex items-center flex-wrap gap-x-2">
@@ -68,16 +66,16 @@ export default function Response({ response, key, index, user, blogAuthorId }) {
                     {/* scenario 2 */}
                     {blogAuthorId == user?.id && user?.role !== "admin" ? (
                       <>
-                        <FlagButton />
                         <HideButton />
+                        <FlagButton />
                       </>
                     ) : (
                       // scenario 3
                       <>
                         {user.role === "admin" ? (
                           <>
-                            <FlagButton />
                             <HideButton />
+                            <FlagButton />
                             <DeleteBtn />
                           </>
                         ) : (
--- components/ui/Buttons.jsx
+++ components/ui/Buttons.jsx
@@ -5,7 +5,7 @@ export const EditButton = (handleClick) => (
     onClick={handleClick}
     className="flex items-center gap-2 text-sm hover:text-white px-1 py-0.5
     rounded-md hover:bg-blue-500"
-    title="edit comment">
+    title="edit response">
     <Edit size={14} />
     <span>Edit</span>
   </button>
@@ -15,7 +15,7 @@ export const HideButton = (handleClick) => (
   <button
     onClick={handleClick}
     className="flex items-center gap-2 text-sm   hover:text-white px-1 py-0.5 rounded-md hover:bg-blue-500"
-    title="hide comment">
+    title="hide response">
     <svg viewBox="0 0 64 64" fill="currentColor" height="14" width="14">
       <path
         fill="none"
@@ -58,7 +58,7 @@ export const FlagButton = (handleClick) => (
         d="M12 0v64M12 6h41l-6 12 6 12H12"
       />
     </svg>
-    <span>Flag</span>
+    <span className="xsm:hidden">Flag</span>
   </button>
 );
 
@@ -74,13 +74,15 @@ export const DeleteBtn = (handleClick) => (
 );
 
 export const ReportAbuseBtn = () => (
-  <button className="text-sm hover:text-white  px-1 py-0.5 rounded-md hover:bg-red-400 flex items-center gap-1">
+  <button
+    className="text-sm hover:text-white  px-1 py-0.5 rounded-md hover:bg-red-400 flex items-center gap-1"
+    title="report abuse">
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
       height="14"
       width="14"
-      className="group-hover:text-red-500">
+      className="group-hover:text-red-500 xsm:text-xs">
       <path fill="none" d="M0 0h24v24H0z" />
       <path d="M4 20v-6a8 8 0 1116 0v6h1v2H3v-2h1zm2 0h12v-6a6 6 0 10-12 0v6zm5-18h2v3h-2V2zm8.778 2.808l1.414 1.414-2.12 2.121-1.415-1.414 2.121-2.121zM2.808 6.222l1.414-1.414 2.121 2.12L4.93 8.344 2.808 6.222zM7 14a5 5 0 015-5v2a3 3 0 00-3 3H7z" />
     </svg>
