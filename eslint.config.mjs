c8b4e874091ec778c89bf5eeca12955b46cf541d
donvine <donvinemugendi@gmail.com>
2025-06-20 15:49:07 +0300

    feat: update ESLint configuration to ignore generated Prisma files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["generated/prisma/**"],
+  },
 ];
 
 export default eslintConfig;
