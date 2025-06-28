e233af7837ff2d524ba3101b09a04919af66206e
Donvine254 <donvinemugendi@gmail.com>
2025-06-29 01:13:38 +0300

    fix: improve input validation styles and update link component in login form

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -20,22 +20,22 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"div">) 
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="m@example.com" required />
+                                <Input id="email" type="email" className="invalid:not-[]:" placeholder="m@example.com" required />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
                                     <Label htmlFor="password">Password</Label>
-                                    <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
+                                    <Link href="/reset" className="ml-auto text-sm underline-offset-2 hover:underline">
                                         Forgot your password?
-                                    </a>
+                                    </Link>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input id="password" type="password" required minLength={4} placeholder="********" />
                             </div>
                             <Button type="submit" className="w-full">
                                 Login
                             </Button>
                             <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                                <span className="relative z-10 bg-background px-2 text-muted-foreground">Or continue with</span>
+                                <span className="relative z-10 bg-background  px-2 text-muted-foreground">Or continue with</span>
                             </div>
                             <div className="grid grid-cols-3 gap-4">
                                 <Button variant="outline" className="w-full" title="login with google">
