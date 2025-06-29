184d802b9dae2992b99bc1578ebc3460afb7824e
Donvine254 <donvinemugendi@gmail.com>
2025-06-29 21:58:05 +0300

    feat: implement rate limiting for user login attempts and add bcrypt for password validation

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,6 +1,8 @@
 "use server";
 import prisma from "@/prisma/prisma";
+import * as bcrypt from "bcrypt";
 import { createAndSetAuthTokenCookie } from "./jwt";
+import { rateLimitByIp } from "./rate-limiter";
 export async function authenticateSSOLogin(email: string) {
   try {
     const user = await prisma.user.findUnique({
@@ -15,5 +17,53 @@ export async function authenticateSSOLogin(email: string) {
   } catch (error) {
     console.error(error);
     return { success: false, error: "Something went wrong" };
+  }
+}
+
+export async function authenticateUserLogin(
+  email: string,
+  password: string,
+  ip: string
+) {
+  try {
+    // step-1: check the login attempts
+    const rateCheck = rateLimitByIp(ip);
+    if (!rateCheck.allowed) {
+      return { success: false, message: rateCheck.message };
+    }
+    //step-2: check if user exists
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email.toLowerCase(),
+      },
+    });
+    if (!user) {
+      return { success: false, message: "user not found" };
+    }
+    if (!user.email_verified) {
+      return {
+        success: false,
+        message: "Email not verified. Verify your account to login",
+      };
+    }
+    //step-3: validate passwords
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user.password_digest
+    );
+    if (!isPasswordValid) {
+      return { success: false, message: "Wrong password, try again" };
+      // call ip limiter and add one attempt
+      //rateLimitByIp(ip)
+    }
+    //step-4: auth success: create cookie
+    await createAndSetAuthTokenCookie(user);
+    return { success: true, message: "Logged in successfully" };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Unexpected error occured!",
+    };
   }
 }
