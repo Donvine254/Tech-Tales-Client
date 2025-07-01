277f8e40b4244f0959233215534d2373c126cdfb
Donvine254 <donvinemugendi@gmail.com>
2025-07-01 21:27:17 +0300

    feat: add voice search functionality and integrate tooltip for voice command button in SearchBar component

--- components/custom/search.tsx
+++ components/custom/search.tsx
@@ -14,6 +14,13 @@ import {
 import { DialogTitle } from "../ui/dialog";
 import { categories } from "@/constants";
 import { Button } from "../ui/button";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { toast } from "sonner";
 
 export default function SearchBar({ className }: { className: string }) {
     const [open, setOpen] = React.useState(false);
@@ -44,6 +51,34 @@ export default function SearchBar({ className }: { className: string }) {
             setQuery("");
         }
     }, [router]);
+    // function to search by voice
+    const handleVoiceSearch = () => {
+        /*eslint-disable */
+        if (typeof window !== undefined) {
+            return false
+        }
+        // check for browser support
+        if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
+            const SpeechRecognition = (window as any).webkitSpeechRecognition || (window as any).SpeechRecognition;
+            const recognition = new SpeechRecognition();
+            recognition.continuous = false;
+            recognition.interimResults = false;
+            recognition.lang = 'en-US';
+            recognition.onresult = (event: any) => {
+                const transcript = event.results[0][0].transcript;
+                setQuery(transcript);
+                setTimeout(() => {
+                    handleSearch(transcript);
+                }, 2000)
+            };
+            recognition.onerror = (event: any) => {
+                console.error('Speech recognition error:', event.error);
+            };
+            recognition.start();
+        } else {
+            toast.error('Speech recognition is not supported in this browser.');
+        }
+    };
     return (
         <div className={className}>
             <Button
@@ -59,15 +94,47 @@ export default function SearchBar({ className }: { className: string }) {
             </Button>
             <CommandDialog open={open} onOpenChange={setOpen} className="dark:bg-gray-900">
                 <DialogTitle />
-                <CommandInput placeholder="Search articles..." value={query} className="truncate text-ellipsis"
-                    onValueChange={setQuery} onKeyDown={(e) => {
-                        if (e.key === "Enter" && query.trim()) {
-                            handleSearch(query);
-                        }
-                    }} />
+                <div className="relative">
+                    <CommandInput placeholder="Search articles..." value={query} className="truncate pr-12 text-ellipsis "
+                        onValueChange={setQuery} onKeyDown={(e) => {
+                            if (e.key === "Enter" && query.trim()) {
+                                handleSearch(query);
+                            }
+                        }} />
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    size="sm"
+                                    className="h-8 w-8 p-0 absolute right-8 top-1/2 -translate-y-1/2 group hover:bg-gray-50 dark:hover:bg-gray-700 rounded-full"
+                                    onClick={handleVoiceSearch}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="1rem"
+                                        height="1rem"
+                                        className="h-4 w-4 group-hover:text-blue-500"
+                                    >
+                                        <path
+                                            fill="currentColor"
+                                            d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3m5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72z"
+                                        ></path>
+                                    </svg>
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent className="max-w-72 text-sm" side="bottom">
+                                <p>Search with your voice</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                </div>
+
                 <CommandList>
                     <CommandEmpty className="p-2">
-                        <Button variant="outline" className=" cursor-pointer w-full relative flex items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none bg-accent capitalize border-none"
+                        <Button variant="outline" className="cursor-pointer w-full relative flex items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none bg-accent capitalize border-none"
                             onClick={() => {
                                 handleSearch(query.trim());
                             }}> <Search className="mr-2 h-4 w-4" />
