3940214229d8d42462099a6e5f570d60f70f9d01
Donvine254 <donvinemugendi@gmail.com>
2025-07-04 08:13:06 +0300

    fix: update button variant for category filters and improve formatting

--- components/pages/category-filters.tsx
+++ components/pages/category-filters.tsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import { ScrollArea, ScrollBar } from '../ui/scroll-area'
-import Link from 'next/link'
-import { categories } from '@/constants'
-import { Button } from '../ui/button'
+import React from "react";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
+import Link from "next/link";
+import { categories } from "@/constants";
+import { Button } from "../ui/button";
 
 export default function CategoryFilters() {
-    return (
-        <ScrollArea className="w-full my-8">
-            <div className="flex space-x-2 pb-4">
-                {categories.map((category) => {
-                    return (
-                        <Button
-                            key={category.value}
-                            asChild
-                            variant={category.value === "all" ? "default" : "secondary"}
-                            className={`flex capitalize items-center gap-2 cursor-pointer whitespace-nowrap ${category.value === "all"
-                                ? "bg-primary text-primary-foreground"
-                                : "hover:bg-blue-500 hover:text-white"
-                                }`}>
-                            <Link href={`/search?q=${category.value}`}>{category.label}</Link>
-                        </Button>
-                    );
-                })}
-            </div>
-            <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-    )
+  return (
+    <ScrollArea className="w-full my-8">
+      <div className="flex space-x-2 pb-4">
+        {categories.map((category) => {
+          return (
+            <Button
+              key={category.value}
+              asChild
+              variant={category.value === "all" ? "default" : "outline"}
+              className={`flex capitalize items-center gap-2 cursor-pointer whitespace-nowrap ${
+                category.value === "all"
+                  ? "bg-primary text-primary-foreground"
+                  : "hover:bg-blue-500 hover:text-white"
+              }`}>
+              <Link href={`/search?q=${category.value}`}>{category.label}</Link>
+            </Button>
+          );
+        })}
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  );
 }
