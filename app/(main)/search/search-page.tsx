56f6a2e1b54d9ea91b4e3d0b897fbaaf2f5a0824
Donvine254 <donvinemugendi@gmail.com>
2025-07-07 04:18:56 +0300

    add search pagination

--- app/(main)/search/search-page.tsx
+++ app/(main)/search/search-page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import BlogCard from "@/components/pages/blog-card";
 import CategoryFilters from "@/components/pages/category-filters";
 import { BlogWithComments } from "@/types";
-import { SearchX } from "lucide-react";
+import { SearchX, ChevronLeft, ChevronRight } from "lucide-react";
 import { useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+const BLOGS_PER_PAGE = 6;
 
 export default function SearchPage({
   blogs,
@@ -13,7 +15,7 @@ export default function SearchPage({
 }) {
   const searchParams = useSearchParams();
   const query = searchParams.get("q") || "";
-
+  const [currentPage, setCurrentPage] = useState(1);
   const filteredBlogs = useMemo(() => {
     if (!query) return []; // Return nothing if no search term
 
@@ -29,24 +31,59 @@ export default function SearchPage({
       return titleMatch || tagMatch;
     });
   }, [blogs, query]);
+  //   add pagination
+  const totalPages = Math.ceil(filteredBlogs.length / BLOGS_PER_PAGE);
+  const paginatedBlogs = useMemo(() => {
+    const start = (currentPage - 1) * BLOGS_PER_PAGE;
+    return filteredBlogs.slice(start, start + BLOGS_PER_PAGE);
+  }, [filteredBlogs, currentPage]);
+
+  const handlePrev = () =>
+    setCurrentPage((prev: number) => Math.max(prev - 1, 1));
+  const handleNext = () =>
+    setCurrentPage((prev: number) => Math.min(prev + 1, totalPages));
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:p-8">
       {/* Header section */}
       <div className="py-4 border-b border-border mb-6">
         <h1 className="text-primary/70 font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl font-serif">
           <span>Search results for </span>
-          <span className="text-primary">{query}</span>
+          <span className="text-primary truncate">{query.trim()}</span>
         </h1>
       </div>
       <CategoryFilters className="my-8" />
       {/* search results */}
-      {filteredBlogs && filteredBlogs.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {filteredBlogs &&
-            filteredBlogs.map((post, index: number) => (
+      {paginatedBlogs.length > 0 ? (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {paginatedBlogs.map((post, index) => (
               <BlogCard key={index} blog={post} />
             ))}
-        </div>
+          </div>
+
+          {/* Pagination Controls */}
+          <div className="flex justify-center items-center gap-4 mt-10">
+            <Button
+              onClick={handlePrev}
+              size="sm"
+              variant="ghost"
+              disabled={currentPage === 1}
+              className="cursor-pointer items-center">
+              <ChevronLeft /> Prev
+            </Button>
+            <span className="font-semibold text-sm text-muted-foreground">
+              Page {currentPage} of {totalPages}
+            </span>
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={handleNext}
+              disabled={currentPage === totalPages}
+              className="cursor-pointer items-center">
+              Next <ChevronRight />
+            </Button>
+          </div>
+        </>
       ) : (
         <div className="flex flex-col items-center justify-center text-center py-16 text-primary/70">
           <SearchX className="w-12 h-12 mb-4 text-muted-foreground" />
