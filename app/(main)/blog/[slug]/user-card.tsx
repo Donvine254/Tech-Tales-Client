3d02312c93f3eb9c5b19b02b0368caefd7611b17
Donvine254 <donvinemugendi@gmail.com>
2025-07-08 03:03:29 +0300

    update user card sizing

--- app/(main)/blog/[slug]/user-card.tsx
+++ app/(main)/blog/[slug]/user-card.tsx
@@ -154,7 +154,7 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
             </div>
           </div>
           <div className="px-2 py-2">
-            <p className="font-medium font-serif text-sm leading-loose my-2">
+            <p className="font-medium font-serif text-sm max-w-[250px] leading-loose my-2">
               {author.bio ?? "This author has not updated their bio yet"}
             </p>
             {author.socials?.length ? (
