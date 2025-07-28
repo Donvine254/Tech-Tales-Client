713ec8275fd8fa37aed32b04dd18d74d0e7ee278
Donvine254 <donvinemugendi@gmail.com>
2025-07-28 23:20:05 +0300

    update account recovery page

--- app/(auth)/recover/page.tsx
+++ app/(auth)/recover/page.tsx
@@ -1,12 +1,13 @@
+"use client";
 import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 export default function Page() {
   const searchParams = useSearchParams();
-  const query = searchParams.get("query") || "";          
+  const token = searchParams.get("token") || "";
   return (
     <Suspense fallback={<div>loading..</div>}>
       <div>
-        <p>{query}</p>
+        <p>{token}</p>
       </div>
     </Suspense>
   );
