7fcdc69ab0014893d24a2fcafd48be9b5dc78c79
Donvine254 <donvinemugendi@gmail.com>
2025-07-28 23:24:27 +0300

    feat: adding loading suspense boundary in recover page

--- app/(auth)/recover/page.tsx
+++ app/(auth)/recover/page.tsx
@@ -1,14 +1,11 @@
 "use client";
-import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 export default function Page() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token") || "";
   return (
-    <Suspense fallback={<div>loading..</div>}>
-      <div>
-        <p>{token}</p>
-      </div>
-    </Suspense>
+    <div>
+      <p>{token}</p>
+    </div>
   );
 }
