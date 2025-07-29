01aba97c5f4ce864f505bc83a26a1e97c71cd749
Donvine254 <donvinemugendi@gmail.com>
2025-07-29 07:27:11 +0300

    update archived comments

--- app/(main)/me/replies/replies.tsx
+++ app/(main)/me/replies/replies.tsx
@@ -15,6 +15,7 @@ import {
   ChevronRight,
   FileText,
   Flag,
+  InfoIcon,
   ListFilterIcon,
   MessageSquare,
   Search,
@@ -199,6 +200,23 @@ export default function Replies({ data }: { data: UserComments }) {
             </SelectContent>
           </Select>
         </div>
+        {activeTab === "ARCHIVED" && (
+          <div
+            data-slot="alert"
+            role="alert"
+            className="relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current animate-scale-in bg-blue-500 text-white dark:bg-blue-900/50">
+            <InfoIcon />{" "}
+            <div className="col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight">
+              Archived Comments
+            </div>
+            <div className="col-start-2 grid  justify-items-start gap-1 text-sm [&_p]:leading-relaxed">
+              <p>
+                Comments are archived when users choose to delete their
+                accounts. You can easily restore archived comments.
+              </p>
+            </div>
+          </div>
+        )}
         <Suspense
           fallback={
             <div className="grid gap-6">
