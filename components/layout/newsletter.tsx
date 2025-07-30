20df5d21619649f359ae2b345eeb7897e67ea75a
Donvine254 <donvinemugendi@gmail.com>
2025-07-30 22:15:44 +0300

    update newsletter

--- components/layout/newsletter.tsx
+++ components/layout/newsletter.tsx
@@ -54,27 +54,30 @@ export default function Newsletter() {
               <span className="relative z-10">Subscribe</span>
             </button>
           </form>
-          <div className="py-1 mt-4 flex items-center justify-center gap-1 max-w-sm mx-auto">
+          <div
+            role="alert"
+            className="relative w-full px-4 py-3 grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-9 sm:has-[>svg]:gap-x-6 gap-y-0.5 [&>svg]:size-12 sm:[&>svg]:size-8 [&>svg]:translate-y-0.5 [&>svg]:text-current items-start mt-4 mx-auto max-w-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 50 50"
-              width="3.125rem"
-              height="3.15rem"
+              width="1rem"
+              height="1rem"
               className="text-blue-500 fill-blue-500">
               <path d="M 25 3 C 18.363281 3 13 8.363281 13 15 L 13 20 L 9 20 C 7.355469 20 6 21.355469 6 23 L 6 47 C 6 48.644531 7.355469 50 9 50 L 41 50 C 42.644531 50 44 48.644531 44 47 L 44 23 C 44 21.355469 42.644531 20 41 20 L 37 20 L 37 15 C 37 8.363281 31.636719 3 25 3 Z M 25 5 C 30.566406 5 35 9.433594 35 15 L 35 20 L 15 20 L 15 15 C 15 9.433594 19.433594 5 25 5 Z M 9 22 L 41 22 C 41.554688 22 42 22.445313 42 23 L 42 47 C 42 47.554688 41.554688 48 41 48 L 9 48 C 8.445313 48 8 47.554688 8 47 L 8 23 C 8 22.445313 8.445313 22 9 22 Z M 25 30 C 23.300781 30 22 31.300781 22 33 C 22 33.898438 22.398438 34.6875 23 35.1875 L 23 38 C 23 39.101563 23.898438 40 25 40 C 26.101563 40 27 39.101563 27 38 L 27 35.1875 C 27.601563 34.6875 28 33.898438 28 33 C 28 31.300781 26.699219 30 25 30 Z" />
             </svg>
-            <p className="text-xs text-muted-foreground">
-              {" "}
-              By subscribing you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline hover:text-blue-600"
-                title="terms">
-                terms and conditions.
-              </Link>
-              &nbsp;We value your privacy and we will never spam you or sell
-              your information.
-            </p>
+            <div className="col-start-2 grid text-start text-balance justify-items-start gap-1 text-sm [&_p]:leading-relaxed">
+              <p className="text-xs text-muted-foreground">
+                By subscribing you agree to our{" "}
+                <Link
+                  href="/terms"
+                  className="underline hover:text-blue-600"
+                  title="terms">
+                  terms and conditions.
+                </Link>
+                &nbsp;We value your privacy and we will never spam you or sell
+                your information.
+              </p>
+            </div>
           </div>
         </div>
       </div>
