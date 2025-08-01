577b330bda78da00c733b084cf6feb58a69aa762
Donvine254 <donvinemugendi@gmail.com>
2025-08-01 19:18:12 +0300

    feat(analytics): add user activity status tracking
    This new function allows tracking of user login and logout events, updating their status as 'ACTIVE' or 'INACTIVE' in the database.

--- lib/actions/analytics.ts
+++ lib/actions/analytics.ts
@@ -29,7 +29,7 @@ export async function getTopAuthor(userId: number): Promise<boolean> {
     return false;
   }
 }
-
+/*function to track blog reads. This function updates the blog as read only after the user stays on the page for more than 30 seconds */
 export async function updateBlogViews(blogId: number) {
   try {
     await prisma.blog.update({
@@ -46,5 +46,22 @@ export async function updateBlogViews(blogId: number) {
   } catch (error) {
     console.error(error);
     return false;
+  }
+}
+
+/*function to track user login activity. This function updates the user status as either active or inactive based on whether they are logging in or logging out */
+export async function updateUserActivityStatus(
+  userId: number,
+  status: "ACTIVE" | "INACTIVE"
+) {
+  try {
+    await prisma.user.update({
+      where: { id: userId },
+      data: {
+        status,
+      },
+    });
+  } catch (error) {
+    console.log(error);
   }
 }
