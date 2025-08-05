efe3db052601d7ec9cd4bc9423074f8539d162e9
Donvine254 <donvinemugendi@gmail.com>
2025-08-05 22:00:12 +0300

    fix(layout): handle content overflow for links and iframes

--- components/pages/slug/slug.tsx
+++ components/pages/slug/slug.tsx
@@ -204,7 +204,7 @@ export default function Slug({ blog }: Props) {
         <BlogSummaryGenerator title={blog.title} blogId={blog.id} />
         {/* blog body */}
         <article
-          className="leading-8 prose lg:prose-lg prose-headings:mt-8 prose-p:mt-4 md:leading-10 subpixel-antialiased blog-body max-w-none mt-4 prose-slate dark:prose-invert prose-headings:font-semibold prose-headings:text-gray-900 dark:prose-headings:text-gray-50 prose-a:text-blue-600 hover:prose-a:text-blue-800 prose-a:underline-offset-4 prose-img:rounded-lg prose-img:shadow-lg prose-img:border prose-img:border-gray-200 dark:prose-img:border-gray-700 prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-pre:rounded-lg prose-pre:p-4 blog"
+          className="leading-8 prose lg:prose-lg prose-headings:mt-8 prose-p:mt-4 md:leading-10 subpixel-antialiased blog-body max-w-none mt-4 prose-slate dark:prose-invert prose-headings:font-semibold prose-headings:text-gray-900 dark:prose-headings:text-gray-50 prose-a:text-blue-600 hover:prose-a:text-blue-800 prose-a:underline-offset-4 prose-img:rounded-lg prose-img:shadow-lg prose-img:border prose-img:border-gray-200 dark:prose-img:border-gray-700 prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 blog prose-pre:rounded-lg prose-pre:p-4 blog"
           id="blog-body">
           <PrismLoader />
           {blog.body ? parse(blog.body) : "Loading..."}
