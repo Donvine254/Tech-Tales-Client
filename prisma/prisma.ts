5bfba346ef30e0b48e0a88035e17ff25865f6592
Donvine254 <donvinemugendi@gmail.com>
2025-08-21 06:16:24 +0300

    feat(prisma): add support for read replicas in Prisma client

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { readReplicas } from "@prisma/extension-read-replicas";
 
-const client = new PrismaClient().$extends(withAccelerate());
+const client = new PrismaClient().$extends(withAccelerate()).$extends(
+  readReplicas({
+    url: process.env.DATABASE_REPLICA_URL as string,
+  })
+);
 
 type ExtendedPrismaClient = typeof client;
 
