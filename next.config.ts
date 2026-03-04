af5197a0ddb18eb4c250d48a4e38ef30a00deaf6
Donvine254 <donvinemugendi@gmail.com>
2026-03-04 20:38:29 +0300

    add image quality patterns

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    qualities: [25, 50, 75, 100],
     remotePatterns: [
       {
         protocol: "https",
