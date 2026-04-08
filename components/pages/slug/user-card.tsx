a44de2d9ad8ff957e8f9ac8f14d131a877dedd0c
Donvine254 <donvinemugendi@gmail.com>
2026-04-09 00:04:31 +0300

    update user card styling

--- components/pages/slug/user-card.tsx
+++ components/pages/slug/user-card.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/tooltip";
 import { FC, useState } from "react";
 import { SocialLinks } from "@/components/custom/social-links";
+import { Button } from "@/components/ui/button";
 
 interface SocialLink {
   platform: string;
@@ -54,17 +55,17 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
           </AvatarFallback>
         </Avatar>
       </TooltipTrigger>
-      <TooltipContent className="w-fit min-w-[250px] max-w-72" side="bottom">
-        <div className="flex items-center gap-2 w-full rounded-t-md px-4 py-2 border-b border-gray-600 dark:border-gray-300">
+      <TooltipContent className="w-fit min-w-62.5 max-w-72" side="bottom">
+        <div className="flex flex-col items-center gap-2 w-full rounded-t-md px-4 pt-2 ">
           <Link href={`/explore/${author.handle}`} prefetch>
             <Avatar
-              className="h-10 md:h-12 w-10 md:w-12 ring-2 ring-offset-2 cursor-pointer"
+              className="h-10 md:h-16 lg:h-20 w-10 md:w-16 lg:w-20 ring-2 ring-offset-2 cursor-pointer"
               style={{ boxShadow: `0 0 0 2px ${author.branding}` }}>
               <AvatarImage
                 src={author.picture ?? "/placeholder.svg"}
                 alt={author.username}
               />
-              <AvatarFallback className="bg-gradient-to-r from-cyan-100 to-blue-100 text-cyan-700 text-sm">
+              <AvatarFallback className="bg-linear-to-r from-cyan-100 to-blue-100 text-cyan-700 text-sm">
                 {author.username
                   .split(" ")
                   .map((n) => n[0])
@@ -73,7 +74,7 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
             </Avatar>
           </Link>
           <div className="capitalize font-bold text-lg">
-            <p className="w-full flex items-center gap-1">
+            <div className="w-full flex items-center gap-1">
               <Link href={`/explore/${author.handle}`} prefetch>
                 {author.username}
               </Link>
@@ -83,7 +84,7 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
                   width={18}
                   height={18}
                   alt="verification-badge"
-                  className="h-auto w-auto max-w-[18px] mb-1"
+                  className="h-auto w-auto max-w-4.5 mb-1"
                 />
               ) : (
                 <svg
@@ -92,16 +93,28 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
                   height="18"
                   viewBox="0 0 24 24"
                   fill="#1D9BF0">
+                  <title>badge</title>
                   <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
                   <path d="m9 12 2 2 4-4" stroke="#ffffff" />
                 </svg>
               )}
+            </div>
+            <p className="font-normal text-xs sm:text-sm text-center lowercase dark:text-blue-500">
+              @{author.handle}
             </p>
           </div>
         </div>
-        <div className="px-2 py-2">
-          <p className="font-medium font-serif text-sm leading-loose my-2">
-            {author.bio ?? "This author has not updated their bio yet"}
+        <div className="p-2">
+          <p className="text-xs sm:text-sm leading-loose mb-2">
+            {author.bio && author.bio.trim() !== "" ? (
+              author.bio
+            ) : (
+              <>
+                <span className="capitalize">{author.username}</span> is a
+                contributor at Techtales, sharing insights, stories, and guides
+                about technology and the digital world.
+              </>
+            )}
           </p>
           {author.socials?.length ? (
             <div className="space-y-3">
@@ -112,13 +125,15 @@ const UserCard: FC<UserCardProps> = ({ author }) => {
             </div>
           ) : null}
         </div>
-        <hr className="border border-gray-600 dark:border-gray-300" />
-        <Link
-          href={`/explore/${author.handle}`}
-          prefetch
-          className="text-sm text-blue-500 hover:text-sky-600 hover:underline px-2 py-2 block">
-          More from this author &#8599;
-        </Link>
+
+        <Button asChild className="w-full" variant="secondary">
+          <Link
+            href={`/explore/${author.handle}`}
+            prefetch
+            className="capitalize">
+            More from {author.username}
+          </Link>
+        </Button>
       </TooltipContent>
     </Tooltip>
   );
